23387884dc76b496f6cd49f17de6523b4b3757a3
kervala <none@none>
2010-07-08 17:26:34 +0200

    Changed: #841 Change the wrong header in the source code

--- code/nel/tools/georges/georges2csv/georges2csv.cpp
+++ code/nel/tools/georges/georges2csv/georges2csv.cpp
@@ -1,5 +1,19 @@
-// sadge_test.cpp : Defines the entry point for the console application.
+// NeL - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 /*
 
 	Script commands:
@@ -872,7 +886,7 @@ void	convertCsvFile( const string &file, bool generate, const string& sheetType 
 					nlwarning( "Sheet %s not found", filebase.c_str( )); 
 					continue;
 				}
-			}			
+			}
 			else
 			{
 				// Load template sheet
