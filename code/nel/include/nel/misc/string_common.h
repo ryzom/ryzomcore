e9492506b14cce219fd6c63120e2f773e643aab9
vl <none@none>
2010-09-21 09:25:53 +0200

    Fixed: compile on linux

--- code/nel/include/nel/misc/string_common.h
+++ code/nel/include/nel/misc/string_common.h
@@ -205,7 +205,9 @@ inline std::string toString(const long unsigned int &val)
 #if (SIZEOF_SIZE_T) == 8
 inline std::string toString(const size_t &val) { return toString("%"NL_I64"u", val); }
 #else
+#ifdef NL_OS_MAC
 inline std::string toString(const size_t &val) { return toString("%u", val); }
+#endif
 #endif
 inline std::string toString(const float &val) { return toString("%f", val); }
 inline std::string toString(const double &val) { return toString("%lf", val); }
