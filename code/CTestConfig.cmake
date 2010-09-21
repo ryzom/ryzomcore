3d842b3d99b03709d3759961f64d3a26dac7919b
"Matt Raykowski" <matt.raykowski@gmail.com>
2010-09-21 14:50:10 -0500

    Changed: Altered build to allow CI builds to be submitted to CDash

--- code/CTestConfig.cmake
+++ code/CTestConfig.cmake
@@ -4,10 +4,10 @@
 ## # The following are required to uses Dart and the Cdash dashboard
 ##   ENABLE_TESTING()
 ##   INCLUDE(Dart)
-set(CTEST_PROJECT_NAME "NeL")
-set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
+set(CTEST_PROJECT_NAME "RyzomCore")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.opennel.org")
-set(CTEST_DROP_LOCATION "/cdash/submit.php?project=NeL")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=RyzomCore")
 set(CTEST_DROP_SITE_CDASH TRUE)
