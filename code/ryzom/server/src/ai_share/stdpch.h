2c09b73e7c8abecb13b71da86fcbc30e1db24fbc
kervala <none@none>
2010-09-25 16:04:53 +0200

    Changed: Optimized PCH for ai_share

--- code/ryzom/server/src/ai_share/stdpch.h
+++ code/ryzom/server/src/ai_share/stdpch.h
@@ -17,6 +17,44 @@
 #ifndef AI_SHARE_STDPCH_H
 #define AI_SHARE_STDPCH_H
 
-#include <nel/misc/types_nl.h>
+#include "nel/misc/types_nl.h"
+
+#include <errno.h>
+#include <limits>
+#include <string>
+#include <vector>
+
+#include "nel/misc/algo.h"
+#include "nel/misc/command.h"
+#include "nel/misc/common.h"
+#include "nel/misc/config_file.h"
+#include "nel/misc/debug.h"
+#include "nel/misc/entity_id.h"
+#include "nel/misc/file.h"
+#include "nel/misc/hierarchical_timer.h"
+#include "nel/misc/i_xml.h"
+#include "nel/misc/path.h"
+#include "nel/misc/random.h"
+#include "nel/misc/sheet_id.h"
+#include "nel/misc/singleton.h"
+#include "nel/misc/smart_ptr.h"
+#include "nel/misc/stream.h"
+#include "nel/misc/string_common.h"
+#include "nel/misc/string_conversion.h"
+#include "nel/misc/string_mapper.h"
+#include "nel/misc/time_nl.h"
+#include "nel/misc/vector_2d.h"
+#include "nel/misc/vectord.h"
+
+#include "nel/ligo/ligo_config.h"
+#include "nel/ligo/primitive.h"
+#include "nel/ligo/primitive_utils.h"
+
+#include "nel/net/message.h"
+#include "nel/net/service.h"
+#include "nel/net/transport_class.h"
+#include "nel/net/unified_network.h"
+
+#include "nel/pacs/u_global_position.h"
 
 #endif
