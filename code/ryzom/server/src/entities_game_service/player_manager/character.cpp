a1a6c669f2b01d70fc5d9bed01b7c332f4f6d9d0
kervala <none@none>
2010-11-02 21:49:34 +0100

    Changed: #878 Fix typos in comments/code

--- code/ryzom/server/src/entities_game_service/player_manager/character.cpp
+++ code/ryzom/server/src/entities_game_service/player_manager/character.cpp
@@ -5704,7 +5704,6 @@ bool CCharacter::onAnimalHungry( uint petIndex, bool justBecameHungry )
 				// Consume to full satiety (last useful unit is entirely consumed)
 				animal.Satiety = animal.MaxSatiety;
 				nbUnits = (sint)ceil(caloriesNeeded / caloriesPerUnit);
-
 			}
 			nbItemsLeftToConsume -= nbUnits;
 
