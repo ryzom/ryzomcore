2efa82be4eef7cfd6fc61566b4a8cdacc6b94690
kervala <none@none>
2010-11-18 21:35:17 +0100

    Changed: #841 Change the wrong header in the source code

--- code/ryzom/common/src/game_share/backup_service_messages.h
+++ code/ryzom/common/src/game_share/backup_service_messages.h
@@ -1,6 +1,18 @@
-/** file backup_service_messages.h
- * $Id: backup_service_messages.h,v 1.3 2007/03/09 09:56:52 boucher Exp $
- */
+// NeL - MMORPG Framework <http://dev.ryzom.com/projects/nel/>
+// Copyright (C) 2010  Winch Gate Property Limited
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef BACKUP_SERVICE_MESSAGES_H
 #define	BACKUP_SERVICE_MESSAGES_H
