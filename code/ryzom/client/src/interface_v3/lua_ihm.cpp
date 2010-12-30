2c5001ee1b4b4232a1c9eda32892557ddb67cf67
kervala <none@none>
2010-12-30 12:20:42 +0100

    Fixed: Compilation with luabind 0.7 (include luabind/version.hpp only when existing)

--- code/ryzom/client/src/interface_v3/lua_ihm.cpp
+++ code/ryzom/client/src/interface_v3/lua_ihm.cpp
@@ -16,15 +16,9 @@
 
 
 #include "stdpch.h"
-#include <algorithm>
-
 #include "lua_helper.h"
-// Warning: cannot use namespace std,    when using luabind
-#ifdef NL_OS_WINDOWS
-#  ifndef NL_EXTENDED_FOR_SCOPE
-#    undef for
-#  endif
-#endif
+
+#include <algorithm>
 
 // to get rid of you_must_not_use_assert___use_nl_assert___read_debug_h_file messages
 #include <cassert>
@@ -32,26 +26,36 @@
 	#undef assert
 #endif
 
+// Warning: cannot use namespace std,    when using luabind
+#ifdef NL_OS_WINDOWS
+#  ifndef NL_EXTENDED_FOR_SCOPE
+#    undef for
+#  endif
+#endif
+
 #ifdef NL_DEBUG
-	#define assert(x) nlassert(x)
+#	define assert(x) nlassert(x)
 #else
-	#define assert(x)
+#	define assert(x)
 #endif
 
 #include <luabind/luabind.hpp>
 // in luabind > 0.6, LUABIND_MAX_ARITY is set to 10
 #if LUABIND_MAX_ARITY == 10
-# include <luabind/operator.hpp>
-# include <luabind/version.hpp>
-# ifndef LUABIND_VERSION
+#	include <luabind/operator.hpp>
+// only luabind > 0.7 have version.hpp (file checked with build system)
+#	ifdef HAVE_LUABIND_VERSION
+#		include <luabind/version.hpp>
+#	endif
+#	ifndef LUABIND_VERSION
 // luabind 0.7 doesn't define LUABIND_VERSION
-#   define LUABIND_VERSION 700
-# endif
+#		define LUABIND_VERSION 700
+#	endif
 // luabind 0.6 doesn't define LUABIND_VERSION but LUABIND_MAX_ARITY is set to 5
 #elif LUABIND_MAX_ARITY == 5
-# define LUABIND_VERSION 600
+#	define LUABIND_VERSION 600
 #else
-# pragma error("luabind version not recognized")
+#	pragma error("luabind version not recognized")
 #endif
 
 
