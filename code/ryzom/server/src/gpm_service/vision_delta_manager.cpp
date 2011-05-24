14622fdf7dc01394f6fde14634b012527ec916a0
kervala <none@none>
2011-05-24 10:54:17 +0200

    Changed: #1272 Fixed gpm_service compilation with GCC 4.6 (and added PCH to project)

--- code/ryzom/server/src/gpm_service/vision_delta_manager.cpp
+++ code/ryzom/server/src/gpm_service/vision_delta_manager.cpp
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "stdpch.h"
+
 #include "nel/net/unified_network.h"
 #include "vision_delta_manager.h"
 #include "gpm_service.h"
