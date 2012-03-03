c6a2534c98031ea601f479ba89d2805ec78456d7
kaetemi <none@none>
2012-03-03 20:14:21 +0100

    Changed: #1440 Remove redundant text
    --HG--
    branch : build_pipeline_v3

--- code/nel/tools/pipeline/service/example_cfg/example_run_master.bat
+++ code/nel/tools/pipeline/service/example_cfg/example_run_master.bat
@@ -1,2 +1,2 @@
 cd R:\build\dev\bin\Release\
-start R:\build\dev\bin\Release\pipeline_service.exe -CR:\code\nel\tools\pipeline\service\example_cfg\master\
+start pipeline_service.exe -CR:\code\nel\tools\pipeline\service\example_cfg\master\
