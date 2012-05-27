f4781db64b71878bb2fd56d7b32ee84c97cfbbca
dfighter1985 <none@none>
2012-05-27 21:52:02 +0200

    ADDED: #1471 OVQT GUI Editor plugin skeleton code.
    --HG--
    branch : gsoc2012-gui-editor

---
+++ code/nel/tools/3d/object_viewer_qt/src/plugins/gui_editor/gui_editor_window.cpp
@@ -0,0 +1,86 @@
+// Object Viewer Qt GUI Editor plugin <http://dev.ryzom.com/projects/ryzom/>
+// Copyright (C) 2010  Winch Gate Property Limited
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "gui_editor_window.h"
+#include "gui_editor_constants.h"
+
+#include "../core/icore.h"
+#include "../core/core_constants.h"
+
+#include <nel/misc/debug.h>
+
+#include <QtCore/QSettings>
+#include <QtGui/QFileDialog>
+
+namespace GUIEditor
+{
+	QString _lastDir;
+	
+	GUIEditorWindow::GUIEditorWindow(QWidget *parent) :
+	QMainWindow(parent)
+	{
+		m_ui.setupUi(this);
+		m_undoStack = new QUndoStack(this);
+		createMenus();
+		readSettings();
+	}
+	
+	GUIEditorWindow::~GUIEditorWindow()
+	{
+		writeSettings();
+	}
+	
+	QUndoStack *GUIEditorWindow::undoStack() const
+	{
+		return m_undoStack;
+	}
+	
+	void GUIEditorWindow::open()
+	{
+		QStringList fileNames = QFileDialog::getOpenFileNames(this,
+											tr("Open GUI XML files"),
+											_lastDir,
+											tr("All XML files (*.xml)"));
+		
+		setCursor(Qt::WaitCursor);
+		if(!fileNames.isEmpty())
+		{
+			QStringList list = fileNames;
+			_lastDir = QFileInfo(list.front()).absolutePath();
+		}
+		setCursor(Qt::ArrowCursor);
+	}
+	
+	void GUIEditorWindow::createMenus()
+	{
+	}
+	
+	void GUIEditorWindow::readSettings()
+	{
+		QSettings *settings = Core::ICore::instance()->settings();
+		settings->beginGroup(Constants::GUI_EDITOR_SECTION);
+		settings->endGroup();
+	}
+	
+	void GUIEditorWindow::writeSettings()
+	{
+		QSettings *settings = Core::ICore::instance()->settings();
+		settings->beginGroup(Constants::GUI_EDITOR_SECTION);
+		settings->endGroup();
+		settings->sync();
+	}
+	
+}
