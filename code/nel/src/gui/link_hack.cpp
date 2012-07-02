ec51a4f340c1705ef03d9da149902b46b8c5b60b
dfighter1985 <none@none>
2012-07-02 05:07:13 +0200

    CHANGED: #1471 Moved the basic interface expression user functions into the NELGUI library under the NLGUI namespace.
    --HG--
    branch : gui-refactoring

--- code/nel/src/gui/link_hack.cpp
+++ code/nel/src/gui/link_hack.cpp
@@ -4,11 +4,14 @@
 
 namespace NLGUI
 {
+	void ifexprufct_forcelink();
+
 	/// Necessary so the linker doesn't drop the code of these classes from the library
 	void LinkHack()
 	{
 		CDBViewBar3::forceLink();
 		CDBViewNumber::forceLink();
 		CDBViewQuantity::forceLink();
+		ifexprufct_forcelink();
 	}
 }
