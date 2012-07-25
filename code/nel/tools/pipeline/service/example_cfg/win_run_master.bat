3a7ca88796ec8daaffca7602e5a6ae0abafd2b72
kaetemi <none@none>
2012-07-25 14:21:24 +0200

    Changed: New default configuration values for pipeline
    --HG--
    branch : build_pipeline_v3

--- code/nel/tools/pipeline/service/example_cfg/win_run_master.bat
+++ code/nel/tools/pipeline/service/example_cfg/win_run_master.bat
@@ -1,2 +1,2 @@
-cd R:\build\dev\bin\Release\
-start pipeline_service.exe -CR:\code\nel\tools\pipeline\service\example_cfg\master\
+cd R:\build\devw\bin\Release\
+start pipeline_service.exe -CR:\code\nel\tools\pipeline\service\example_cfg\master_win\
--- code/nel/tools/pipeline/service/example_cfg/win_run_slave.bat
+++ code/nel/tools/pipeline/service/example_cfg/win_run_slave.bat
@@ -1,2 +1,2 @@
-cd R:\build\dev\bin\Release\
-start pipeline_service.exe -CR:\code\nel\tools\pipeline\service\example_cfg\slave\
+cd R:\build\devw\bin\Release\
+start pipeline_service.exe -CR:\code\nel\tools\pipeline\service\example_cfg\slave_win\
