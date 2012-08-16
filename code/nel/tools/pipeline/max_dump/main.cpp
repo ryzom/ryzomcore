15602bb2c60934f90ab9b12713d09abc0e397a85
kaetemi <none@none>
2012-08-17 00:01:19 +0200

    Added: #1440 Temporary experimental tool related to max files
    --HG--
    branch : build_pipeline_v3

---
+++ code/nel/tools/pipeline/max_dump/main.cpp
@@ -0,0 +1,66 @@
+
+#include <gsf/gsf-infile-msole.h>
+#include <gsf/gsf-infile.h>
+#include <gsf/gsf-input-stdio.h>
+#include <gsf/gsf-utils.h>
+#include <gsf/gsf-doc-meta-data.h>
+#include <gsf/gsf-msole-utils.h>
+#include <glib/gi18n.h>
+#include <string.h>
+#include <cstdio>
+
+static const char *filename = "/srv/work/database/interfaces/anims_max/cp_fy_hof_species.max";
+//static const char *filename = "/home/kaetemi/source/minimax/GE_Acc_MikotoBaniere.max";
+
+// int __stdcall WinMain(void *, void *, void *, int)
+int main(int argc, char **argv)
+{
+	printf("Pipeline Max Dump (Temporary Tool)\n");
+
+	char const *me = (argv[0] ? argv[0] : "pipeline_max_dump");
+	g_set_prgname(me);
+	gsf_init();
+
+	GsfInfile *infile;
+	GError *error = NULL;
+	GsfInput *src;
+	char *display_name;
+
+	src = gsf_input_stdio_new(filename, &error);
+
+	if (error)
+	{
+		display_name = g_filename_display_name(filename);
+		g_printerr (_("%s: Failed to open %s: %s\n"),
+			    g_get_prgname (),
+			    display_name,
+			    error->message);
+		g_free(display_name);
+		return 1;
+	}
+
+	infile = gsf_infile_msole_new(src, NULL);
+
+	if (!infile)
+	{
+		display_name = g_filename_display_name(filename);
+		g_printerr (_("%s: Failed to recognize %s as an archive\n"),
+				g_get_prgname (),
+				display_name);
+		g_free (display_name);
+		return 1;
+	}
+
+	display_name = g_filename_display_name(filename);
+	g_print("%s\n", display_name);
+	g_free(display_name);
+
+	GsfInput *input = gsf_infile_child_by_name(infile, "DllDirectory");
+	gsf_input_dump(input, 1); // just a regular hex dump of this input stream
+
+	g_object_unref(input);
+	g_object_unref(infile);
+
+	return 0;
+}
+
