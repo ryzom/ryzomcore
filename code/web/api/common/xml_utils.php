41c00b9b48ea37817f6f368bb320a0c376799773
ulukyn <none@none>
2012-10-23 17:44:36 +0200

    Changed: Update from Ryzom Team

---
+++ code/web/api/common/xml_utils.php
@@ -0,0 +1,23 @@
+<?php
+/* Copyright (C) 2009 Winch Gate Property Limited
+ *
+ * This file is part of ryzom_api.
+ * ryzom_api is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ryzom_api is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ryzom_api.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+function ryzom_display_xml_header() {
+	header('Content-Type: application/xml; charset=UTF-8');
+}
+
+?>
