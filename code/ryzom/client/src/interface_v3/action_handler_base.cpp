33c73f3f16b966ad9602de0faec8f947584475f7
kervala <none@none>
2013-03-26 19:57:00 +0100

    fixes #33 build failure/error using clang

--- code/ryzom/client/src/interface_v3/action_handler_base.cpp
+++ code/ryzom/client/src/interface_v3/action_handler_base.cpp
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "stdpch.h"
 #include "action_handler_base.h"
 #include "action_handler_misc.h"
 
--- code/ryzom/client/src/interface_v3/parser_modules.cpp
+++ code/ryzom/client/src/interface_v3/parser_modules.cpp
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "stdpch.h"
 #include "parser_modules.h"
 #include "nel/gui/view_text.h"
 #include "nel/gui/interface_group.h"
