4438e87a8c3665eccb6fae5eac35d9e913a07bdb
Botanic <none@none>
2013-06-15 07:01:11 -0700

    added charisma
    --HG--
    branch : rc-botanic-webdev

---
+++ code/ryzom/tools/server/ryzom_ams/www/html/index_old.php
@@ -0,0 +1,20 @@
+<?php
+
+require( '../config.php' );
+require( '../../ams_lib/libinclude.php' );
+
+if ( isset( $_POST["function"] ) ){
+     require( "inc/" . $_POST["function"] . ".php" );
+     $return = $_POST["function"]();
+    }
+
+function loadpage ( $page ){
+     require_once( 'autoload/' . $page . '.php' );
+    }
+
+$page = 'home';
+if ( isset( $_GET["page"] ) ){
+     $page = $_GET["page"];
+    }
+
+helpers :: loadTemplate( 'register' , $return );
