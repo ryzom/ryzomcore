321edeff107f9c4bc35af69d36893b2010850f0e
Botanic <none@none>
2013-06-18 10:48:54 -0700

    add_user example
    --HG--
    branch : rc-botanic-webdev

--- code/ryzom/tools/server/ryzom_ams/www/html/inc/add_user.php
+++ code/ryzom/tools/server/ryzom_ams/www/html/inc/add_user.php
@@ -1,13 +1,78 @@
 <?php
 
-
 function add_user(){
+
+    if ( isset( $_POST["Username"] ) and isset( $_POST["Password"] ) and isset( $_POST["Email"] ) )
+    {
+     // check values
+    $user = users :: checkUser( $_POST["Username"] );
+     $pass = users :: checkPassword( $_POST["Password"] );
+     $cpass = users :: confirmPassword();
+     $email = users :: checkEmail( $_POST["Email"] );
+     }else{
+     $user = "";
+     $pass = "";
+     $cpass = "";
+     $email = "";
+     }
+ // if all are good then create user
+if ( ( $user == "success" ) and ( $pass == "success" ) and ( $cpass == "success" ) and ( $email == "success" ) and ( isset( $_POST["TaC"] ) ) ){
+     $edit = array(
+        'name' => $_POST["Username"],
+         'pass' => $_POST["Password"],
+         'mail' => $_POST["Email"],
+         'init' => $_POST["Email"],
+         'unhashpass' => $_POST["Password"],
+         'status' => 1,
+         'access' => REQUEST_TIME
+         );
+     header( 'Location: email_sent.php' );
+     write_user( $edit );
+     exit;
+     }else{
+     $pageElements = array(
+        //'GAME_NAME' => $GAME_NAME,
+        // 'WELCOME_MESSAGE' => $WELCOME_MESSAGE,
+        // 'USERNAME' => $user,
+        // 'PASSWORD' => $pass,
+        // 'CPASSWORD' => $cpass,
+        // 'EMAIL' => $email
+         );
+     if ( $user != "success" ){
+         $pageElements['USERNAME_ERROR'] = 'TRUE';
+         }else{
+         $pageElements['USERNAME_ERROR'] = 'FALSE';
+         }
+
+     if ( $pass != "success" ){
+         $pageElements['PASSWORD_ERROR'] = 'TRUE';
+         }else{
+         $pageElements['PASSWORD_ERROR'] = 'FALSE';
+         }
+     if ( $cpass != "success" ){
+         $pageElements['CPASSWORD_ERROR'] = 'TRUE';
+         }else{
+         $pageElements['CPASSWORD_ERROR'] = 'FALSE';
+         }
+     if ( $email != "success" ){
+         $pageElements['EMAIL_ERROR'] = 'TRUE';
+         }else{
+         $pageElements['EMAIL_ERROR'] = 'FALSE';
+         }
+     if ( isset( $_POST["TaC"] ) ){
+         $pageElements['TAC_ERROR'] = 'FALSE';
+         }else{
+         $pageElements['TAC_ERROR'] = 'TRUE';
+         }
+         helpers :: loadtemplate( 'register', $pageElements );
+     }
+}
+
+
+function write_user(){
+
+
      // add user locally here
-     $return = users :: add_user();
-     return $return;
+     users :: add_user();
      }
 
-function checkUser(){
-
-
-     }
