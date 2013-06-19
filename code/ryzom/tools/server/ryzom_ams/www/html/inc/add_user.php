fb622a9eb73df51d536235e8be3817e07acce35a
Quitta <none@none>
2013-06-19 06:50:08 +0200

    register refactoring + small bug fixes that went unnoticed before
    --HG--
    branch : quitta-gsoc-2013

--- code/ryzom/tools/server/ryzom_ams/www/html/inc/add_user.php
+++ code/ryzom/tools/server/ryzom_ams/www/html/inc/add_user.php
@@ -1,70 +1,30 @@
 <?php
 
 function add_user(){
-
-    if ( isset( $_POST["Username"] ) and isset( $_POST["Password"] ) and isset( $_POST["Email"] ) )
-    {
-     // check values
-    $user = users :: checkUser( $_POST["Username"] );
-     $pass = users :: checkPassword( $_POST["Password"] );
-     $cpass = users :: confirmPassword();
-     $email = users :: checkEmail( $_POST["Email"] );
+     $result = Users :: check_Register();
+     //print_r($result);
+     // if all are good then create user
+     if ( $result == "success"){
+          $edit = array(
+             'name' => $_POST["Username"],
+              'pass' => $_POST["Password"],
+              'mail' => $_POST["Email"],
+              'init' => $_POST["Email"],
+              'unhashpass' => $_POST["Password"],
+              'status' => 1,
+              'access' => REQUEST_TIME
+              );
+          //header( 'Location: email_sent.php' );
+          write_user( $edit );
+          exit;
      }else{
-     $user = "";
-     $pass = "";
-     $cpass = "";
-     $email = "";
-     }
- // if all are good then create user
-if ( ( $user == "success" ) and ( $pass == "success" ) and ( $cpass == "success" ) and ( $email == "success" ) and ( isset( $_POST["TaC"] ) ) ){
-     $edit = array(
-        'name' => $_POST["Username"],
-         'pass' => $_POST["Password"],
-         'mail' => $_POST["Email"],
-         'init' => $_POST["Email"],
-         'unhashpass' => $_POST["Password"],
-         'status' => 1,
-         'access' => REQUEST_TIME
-         );
-     header( 'Location: email_sent.php' );
-     write_user( $edit );
-     exit;
-     }else{
-     $pageElements = array(
-        //'GAME_NAME' => $GAME_NAME,
-        // 'WELCOME_MESSAGE' => $WELCOME_MESSAGE,
-        // 'USERNAME' => $user,
-        // 'PASSWORD' => $pass,
-        // 'CPASSWORD' => $cpass,
-        // 'EMAIL' => $email
-         );
-     if ( $user != "success" ){
-         $pageElements['USERNAME_ERROR'] = 'TRUE';
-         }else{
-         $pageElements['USERNAME_ERROR'] = 'FALSE';
-         }
-
-     if ( $pass != "success" ){
-         $pageElements['PASSWORD_ERROR'] = 'TRUE';
-         }else{
-         $pageElements['PASSWORD_ERROR'] = 'FALSE';
-         }
-     if ( $cpass != "success" ){
-         $pageElements['CPASSWORD_ERROR'] = 'TRUE';
-         }else{
-         $pageElements['CPASSWORD_ERROR'] = 'FALSE';
-         }
-     if ( $email != "success" ){
-         $pageElements['EMAIL_ERROR'] = 'TRUE';
-         }else{
-         $pageElements['EMAIL_ERROR'] = 'FALSE';
-         }
-     if ( isset( $_POST["TaC"] ) ){
-         $pageElements['TAC_ERROR'] = 'FALSE';
-         }else{
-         $pageElements['TAC_ERROR'] = 'TRUE';
-         }
-         helpers :: loadtemplate( 'register', $pageElements );
+          // pass error
+          $result['prevUsername'] = $_POST["Username"];
+          $result['prevPassword'] = $_POST["Password"];
+          $result['prevConfirmPass'] = $_POST["ConfirmPass"];
+          $result['prevEmail'] = $_POST["Email"];
+          $result['no_visible_elements'] = 'TRUE';
+          helpers :: loadtemplate( 'register', $result);
      }
 }
 
@@ -73,6 +33,6 @@ function write_user(){
 
 
      // add user locally here
-     users :: add_user();
+     print('Awesome');
      }
 
