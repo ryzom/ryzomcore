2670e34621cf527a9c6963d0cfd85ff6d19e4f8f
kaetemi <none@none>
2013-07-28 17:59:05 +0200

    Can't include this here. A light header with all of these maximum constants might be handy

--- code/ryzom/client/src/entities.h
+++ code/ryzom/client/src/entities.h
@@ -31,7 +31,6 @@
 #include "user_entity.h"
 // Some constants
 #include "game_share/mission_desc.h"
-#include "interface_v3/people_interraction.h"
 #include "game_share/inventories.h"
 // Misc.
 #include "nel/misc/types_nl.h"
@@ -155,8 +154,8 @@ private:
 
 	// DB node pointers used to update some entity flags
 	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _MissionTargetTitleDB[MAX_NUM_MISSIONS][MAX_NUM_MISSION_TARGETS];
-	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _GroupMemberUidDB[MaxNumPeopleInTeam];
-	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _GroupMemberNameDB[MaxNumPeopleInTeam];
+	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _GroupMemberUidDB[8]; // MaxNumPeopleInTeam in people_interaction.h
+	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _GroupMemberNameDB[8]; // MaxNumPeopleInTeam in people_interaction.h
 	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _BeastUidDB[MAX_INVENTORY_ANIMAL];
 	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _BeastStatusDB[MAX_INVENTORY_ANIMAL];
 	NLMISC::CRefPtr<NLMISC::CCDBNodeLeaf> _BeastTypeDB[MAX_INVENTORY_ANIMAL];
