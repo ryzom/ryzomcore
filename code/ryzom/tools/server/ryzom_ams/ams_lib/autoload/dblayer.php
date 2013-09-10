d055a15f29f06cdd1053de01fafa78f25f90d42b
Quitta <none@none>
2013-09-10 21:26:03 +0200

    added documentation for assigned, dblayer and forwarded class
    --HG--
    branch : quitta-gsoc-2013

--- code/ryzom/tools/server/ryzom_ams/ams_lib/autoload/dblayer.php
+++ code/ryzom/tools/server/ryzom_ams/ams_lib/autoload/dblayer.php
@@ -1,8 +1,19 @@
 <?php
+/**
+* Handles the database connections. It uses PDO to connect to the different databases. It will use the argument of the constructor to setup a connection to the database 
+* with the matching entry in the $cfg global variable.
+* @author Daan Janssens, mentored by Matthew Lagoe
+* 
+*/
 class DBLayer{
     
-    private $PDO;
+    private $PDO; /**< The PDO object, instantiated by the constructor */ 
     
+    /**
+    * The constructor.
+    * Instantiates the PDO object attribute by connecting to the arguments matching database(the db info is stored in the $cfg global var)
+    * @param String, the name of the databases entry in the $cfg global var.
+    */
     function __construct($db)
     {
         global $cfg;
@@ -19,18 +30,35 @@ class DBLayer{
 
     }
     
+    /**
+    * execute a query that doesn't have any parameters
+    * @param $query the mysql query
+    * @return returns a PDOStatement object 
+    */
     public function executeWithoutParams($query){
         $statement = $this->PDO->prepare($query);
         $statement->execute();
         return $statement;
     }
     
+    /**
+    * execute a query that has parameters
+    * @param $query the mysql query
+    * @param $params the parameters that are being used by the query
+    * @return returns a PDOStatement object 
+    */
     public function execute($query,$params){
         $statement = $this->PDO->prepare($query);
         $statement->execute($params);
         return $statement;
     }
     
+    /**
+    * execute a query (an insertion query) that has parameters and return the id of it's insertion
+    * @param $query the mysql query
+    * @param $params the parameters that are being used by the query
+    * @return returns the id of the last inserted element.
+    */
     public function executeReturnId($query,$params){
         $statement = $this->PDO->prepare($query);
         $this->PDO->beginTransaction();
