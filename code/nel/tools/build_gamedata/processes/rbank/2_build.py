240576ab9c01ee4a20e232f3c43609b946e1f395
kaetemi <kaetemi@gmail.com>
2014-02-07 01:01:19 +0100

    Add primitives directory to rbank lookup (needed file_index.cfg - is this the correct primitives dir?)
    --HG--
    branch : build_pipeline_py

--- code/nel/tools/build_gamedata/processes/rbank/2_build.py
+++ code/nel/tools/build_gamedata/processes/rbank/2_build.py
@@ -159,6 +159,8 @@ cf.write("\n")
 cf.write("Pathes = {\n")
 mkPath(log, WorldEditorFilesDirectory);
 cf.write("\t\"" + WorldEditorFilesDirectory + "\", \n");
+mkPath(log, PrimitivesDirectory);
+cf.write("\t\"" + PrimitivesDirectory + "\", \n");
 for dir in IgLookupDirectories:
 	mkPath(log, ExportBuildDirectory + "/" + dir)
 	cf.write("\t\"" + ExportBuildDirectory + "/" + dir + "\", \n")
