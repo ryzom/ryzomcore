74984f768a557c1e485c6af0a9cc485fa5a6ec0c
kaetemi <kaetemi@gmail.com>
2014-02-08 16:24:56 +0100

    Improve leveldesign_dev.bat

--- code/nel/tools/build_gamedata/leveldesign_dev.bat
+++ code/nel/tools/build_gamedata/leveldesign_dev.bat
@@ -1,4 +1,5 @@
-1_export.py -ipj common/gamedev common/data_common common/leveldesign common/exedll common/cfg
-2_build.py -ipj common/gamedev common/data_common common/leveldesign common/exedll common/cfg
-3_install.py -ipj common/gamedev common/data_common common/leveldesign common/exedll common/cfg
+1_export.py -ipj common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg
+2_build.py -ipj common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg
+3_install.py -ipj common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg
 5_client_dev.py
+8_shard_data.py
