bd335d80c402f8409378854550fe071db0699ed7
kaetemi <kaetemi@gmail.com>
2014-02-08 22:23:24 +0100

    Implement skip for zone dependency process

--- code/nel/tools/build_gamedata/configuration/scripts.py
+++ code/nel/tools/build_gamedata/configuration/scripts.py
@@ -82,6 +82,13 @@ def copyFileList(log, dir_source, dir_target, files):
 				if needUpdateLogRemoveDest(log, dir_source + "/" + fileName, dir_target + "/" + fileName):
 					shutil.copy(dir_source + "/" + fileName, dir_target + "/" + fileName)
 
+def copyFileListLogless(log, dir_source, dir_target, files):
+	for fileName in files:
+		if fileName != ".svn" and fileName != ".." and fileName != "." and fileName != "*.*":
+			if (os.path.isfile(dir_source + "/" + fileName)):
+				if needUpdateRemoveDest(log, dir_source + "/" + fileName, dir_target + "/" + fileName):
+					shutil.copy(dir_source + "/" + fileName, dir_target + "/" + fileName)
+
 def copyFileListNoTree(log, dir_source, dir_target, files):
 	for fileName in files:
 		if fileName != ".svn" and fileName != ".." and fileName != "." and fileName != "*.*":
@@ -144,6 +151,9 @@ def copyFilesRecursive(log, dir_source, dir_target):
 
 def copyFiles(log, dir_source, dir_target):
 	copyFileList(log, dir_source, dir_target, os.listdir(dir_source))
+
+def copyFilesLogless(log, dir_source, dir_target):
+	copyFileListLogless(log, dir_source, dir_target, os.listdir(dir_source))
 
 def copyFilesExt(log, dir_source, dir_target, file_ext):
 	files = os.listdir(dir_source)
@@ -288,6 +298,31 @@ def findFile(log, dir_where, file_name):
 				printLog(log, "findFile: file not dir or file?! " + filePath)
 	return ""
 
+def needUpdateDirByLowercaseTagLog(log, dir_source, ext_source, dir_dest, ext_dest):
+	updateCount = 0
+	skipCount = 0
+	lenSrcExt = len(ext_source)
+	sourceFiles = findFilesNoSubdir(log, dir_source, ext_source)
+	destFiles = findFilesNoSubdir(log, dir_dest, ext_dest)
+	for file in sourceFiles:
+		sourceFile = dir_source + "/" + file
+		tagFile = dir_dest + "/" + file[0:-lenSrcExt].lower() + ext_dest
+		if os.path.isfile(tagFile):
+			sourceTime = os.stat(sourceFile).st_mtime
+			tagTime = os.stat(tagFile).st_mtime
+			if (sourceTime > tagTime):
+				updateCount = updateCount + 1
+			else:
+				skipCount = skipCount + 1
+		else:
+			updateCount = updateCount + 1
+	if updateCount > 0:
+		printLog(log, "UPDATE " + str(updateCount) + " / " + str(len(sourceFiles)) + "; SKIP " + str(skipCount) + " / " + str(len(sourceFiles)) + "; DEST " + str(len(destFiles)))
+		return 1
+	else:
+		printLog(log, "SKIP " + str(skipCount) + " / " + str(len(sourceFiles)) + "; DEST " + str(len(destFiles)))
+		return 0
+
 def needUpdateDirByTagLog(log, dir_source, ext_source, dir_dest, ext_dest):
 	updateCount = 0
 	skipCount = 0
@@ -323,6 +358,21 @@ def needUpdateDirNoSubdirFile(log, dir_source, file_dest):
 		if os.path.isfile(filePath):
 			fileTime = os.stat(filePath).st_mtime
 			if fileTime > destTime:
+				return 1
+	else:
+		return 0
+
+def needUpdateFileDirNoSubdir(log, file_source, dir_dest):
+	if not os.path.isfile(file_source):
+		printLog(log, "WARNING MISSING " + file_source)
+		return 0
+	sourceTime = os.stat(file_source).st_mtime
+	destFiles = os.listdir(dir_dest)
+	for file in destFiles:
+		filePath = dir_dest + "/" + file
+		if os.path.isfile(filePath):
+			fileTime = os.stat(filePath).st_mtime
+			if sourceTime > fileTime:
 				return 1
 	else:
 		return 0
