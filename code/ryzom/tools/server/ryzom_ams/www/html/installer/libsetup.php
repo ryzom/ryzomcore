fff4682d92f728ad29c859e2556b18b6719f5ce1
botanic <none@none>
2014-04-07 06:29:44 -0700

    missed it on all of them

--- code/ryzom/tools/server/ryzom_ams/www/html/installer/libsetup.php
+++ code/ryzom/tools/server/ryzom_ams/www/html/installer/libsetup.php
@@ -10,23 +10,23 @@
         echo "failed to get write permissions on logs";
         exit;
     }
-    if(is_writable('../../../admin/graphs_output')) {
+    if(is_writable('../../../../admin/graphs_output')) {
         echo "failed to get write permissions on graphs_output";
         exit;
     }
-    if(is_writable('../../../admin/templates/default_c')) {
+    if(is_writable('../../../../admin/templates/default_c')) {
         echo "failed to get write permissions on default_c";
         exit;
     }
-    if(is_writable('../../www')) {
+    if(is_writable('../../../www')) {
         echo "failed to get write permissions on www";
         exit;
     }
-    if(is_writable('../../www/html/cache')) {
+    if(is_writable('../../../www/html/cache')) {
         echo "failed to get write permissions on cache";
         exit;
     }
-    if(is_writable('../../www/html/templates_c')) {
+    if(is_writable('../../../www/html/templates_c')) {
         echo "failed to get write permissions on templates_c";
         exit;
     }
