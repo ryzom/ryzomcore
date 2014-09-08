e1c70eb42305e3f96ca971e7f969e8b17afced96
botanic <none@none>
2014-09-08 16:23:04 -0700

    dont cache $#1t

--- code/web/public_php/ams/index.php
+++ code/web/public_php/ams/index.php
@@ -33,8 +33,14 @@ if ($NEL_SETUP_VERSION_CONFIGURED < $NEL_SETUP_VERSION) {
 }
 
 require_once( $AMS_LIB . '/libinclude.php' );
+session_cache_limiter('nocache');
 session_start();
-
+header("Expires: Mon, 01 May 2000 06:00:00 GMT");
+header("Last-Modified: ". gmdate("D, d M Y H:i:s") ." GMT");
+header("Cache-Control: max-age=1");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 // Running Cron
 if ( isset( $_GET["cron"] ) ) {
