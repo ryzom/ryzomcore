dfe0b2977aa2952156fe99408bc2a0049f7ea7a3
kaetemi <kaetemi@gmail.com>
2014-11-24 16:07:17 +0100

    Separate version string
    --HG--
    branch : develop

--- code/ryzom/common/src/game_share/ryzom_version.h
+++ code/ryzom/common/src/game_share/ryzom_version.h
@@ -17,7 +17,9 @@
 #ifndef RYZOM_VERSION_H
 #define RYZOM_VERSION_H
 
-#define RYZOM_VERSION	"ryzomcore/v0.11.1-dev"
+#define RYZOM_VERSION	"ryzomcore/" \
+	"v0.11.1" \
+	"-dev"
 
 #endif // RYZOM_VERSION_H
 
