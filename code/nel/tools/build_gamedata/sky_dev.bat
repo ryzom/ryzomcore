0c77c91e95585f5def67fec4e65a44d22de79d06
kaetemi <kaetemi@gmail.com>
2014-12-12 01:22:19 +0100

    Adjust sky batch
    --HG--
    branch : develop

--- code/nel/tools/build_gamedata/sky_dev.bat
+++ code/nel/tools/build_gamedata/sky_dev.bat
@@ -1,9 +1,9 @@
 title Ryzom Core: 1_export.py (LEVELDESIGN)
-1_export.py -ipj common/sky common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg shard/data_shard shard/data_language shard/data_leveldesign shard/data_game_share
+1_export.py -ipj common/sky common/sfx common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg shard/data_shard shard/data_language shard/data_leveldesign shard/data_game_share
 title Ryzom Core: 2_build.py (LEVELDESIGN)
-2_build.py -ipj common/sky common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg shard/data_shard shard/data_language shard/data_leveldesign shard/data_game_share
+2_build.py -ipj common/sky common/sfx common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg shard/data_shard shard/data_language shard/data_leveldesign shard/data_game_share
 title Ryzom Core: 3_install.py (LEVELDESIGN)
-3_install.py -ipj common/sky common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg shard/data_shard shard/data_language shard/data_leveldesign shard/data_game_share
+3_install.py -ipj common/sky common/sfx common/gamedev common/data_common common/data_shard common/leveldesign common/exedll common/cfg shard/data_shard shard/data_language shard/data_leveldesign shard/data_game_share
 title Ryzom Core: b1_client_dev.py (LEVELDESIGN)
 b1_client_dev.py
 title Ryzom Core: b2_shard_data.py (LEVELDESIGN)
