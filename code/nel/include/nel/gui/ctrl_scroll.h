cb94cb4f9f7e9b6c25bbd5a86397a1f109a6c092
Nimetu <nimetu@gmail.com>
2015-12-06 16:26:39 +0200

    Fixed: Scrolling in small steps not working.
    --HG--
    branch : develop

--- code/nel/include/nel/gui/ctrl_scroll.h
+++ code/nel/include/nel/gui/ctrl_scroll.h
@@ -147,7 +147,7 @@ namespace NLGUI
 		sint32 _Aligned; // 0-Top 1-Bottom 2-Left 3-Right
 
 		sint32 _TrackDispPos;
-		sint32 _TrackPos;
+		float  _TrackPos;
 		sint32 _TrackSize;
 		sint32 _TrackSizeMin;
 
