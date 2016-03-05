5a11b80b9fa388d23244a88c925422f143bab85c
kervala <none@none>
2016-03-05 12:35:32 +0100

    Added: Profiles model class
    --HG--
    branch : feature-ryzom-installer

---
+++ code/ryzom/tools/client/ryzom_installer/src/profilesmodel.h
@@ -0,0 +1,26 @@
+#ifndef PROFILESMODEL_H
+#define PROFILESMODEL_H
+
+#include "configfile.h"
+
+class CProfilesModel : public QAbstractListModel
+{
+public:
+	CProfilesModel(QObject *parent);
+	CProfilesModel(const CProfiles &profiles, QObject *parent);
+	virtual ~CProfilesModel();
+
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+	bool removeRows(int row, int count, const QModelIndex & parent = QModelIndex());
+
+	CProfiles& getProfiles() { return m_profiles; }
+
+	bool save() const;
+
+private:
+	CProfiles m_profiles;
+};
+
+#endif
