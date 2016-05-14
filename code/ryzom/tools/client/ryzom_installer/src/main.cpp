c09b1dab319c9544dade2a80fcfc283d3ce0f569
kervala <none@none>
2016-05-14 18:53:09 +0200

    Changed: Check for memory leaks
    --HG--
    branch : feature-ryzom-installer

--- code/ryzom/tools/client/ryzom_installer/src/main.cpp
+++ code/ryzom/tools/client/ryzom_installer/src/main.cpp
@@ -50,7 +50,7 @@
 int main(int argc, char *argv[])
 {
 #if defined(_MSC_VER) && defined(_DEBUG)
-//	_CrtSetDbgFlag (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
 	NLMISC::CApplicationContext appContext;
