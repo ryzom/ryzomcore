32f682d5dc0d0b70e136cb281863382c85b2fc0a
kervala <none@none>
2016-09-12 14:39:07 +0200

    Fixed: Compilation under OS X in 32 bits
    --HG--
    branch : develop

--- code/nel/src/3d/driver/opengl/GL/glext.h
+++ code/nel/src/3d/driver/opengl/GL/glext.h
@@ -4657,8 +4657,13 @@ GLAPI void APIENTRY glVertexBlendARB (GLint count);
 
 #ifndef GL_ARB_vertex_buffer_object
 #define GL_ARB_vertex_buffer_object 1
+#if defined(__APPLE__)
+typedef intptr_t GLsizeiptrARB;
+typedef intptr_t GLintptrARB;
+#else
 typedef ptrdiff_t GLsizeiptrARB;
 typedef ptrdiff_t GLintptrARB;
+#endif
 #define GL_BUFFER_SIZE_ARB                0x8764
 #define GL_BUFFER_USAGE_ARB               0x8765
 #define GL_ARRAY_BUFFER_ARB               0x8892
