7422334e97ac97aed2378dabd4eed0f1176aa4e0
kervala <none@none>
2016-09-16 19:24:43 +0200

    Changed: Only call CoUninitialize if CoInitialize(Ex) succeeded
    --HG--
    branch : develop

--- code/ryzom/tools/client/ryzom_installer/src/main.cpp
+++ code/ryzom/tools/client/ryzom_installer/src/main.cpp
@@ -80,6 +80,26 @@ bool copyInstallerFiles(const QStringList &files, const QString &destination)
 	return true;
 }
 
+#ifdef Q_OS_WIN
+class CCOMHelper
+{
+	bool m_mustUninit;
+
+public:
+	CCOMHelper()
+	{
+		// to fix the bug with QFileDialog::getExistingDirectory hanging under Windows
+		m_mustUninit = SUCCEEDED(CoInitialize(NULL));
+	}
+
+	~CCOMHelper()
+	{
+		// only call CoUninitialize if CoInitialize succeeded
+		if (m_mustUninit) CoUninitialize();
+	}
+};
+#endif
+
 int main(int argc, char *argv[])
 {
 #if defined(_MSC_VER) && defined(_DEBUG)
@@ -87,8 +107,7 @@ int main(int argc, char *argv[])
 #endif
 
 #ifdef Q_OS_WIN
-	// to fix the bug with QFileDialog::getExistingDirectory hanging under Windows
-	CoInitialize(NULL);
+	CCOMHelper comHelper;
 #endif
 
 	NLMISC::CApplicationContext appContext;
