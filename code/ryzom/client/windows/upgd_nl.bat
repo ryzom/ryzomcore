338c1883bcd951f8f1f2aff3fabf5313617488d6
kervala <none@none>
2016-09-21 18:04:07 +0200

    Fixed: Don't ask confirmation
    --HG--
    branch : develop

--- code/ryzom/client/windows/upgd_nl.bat
+++ code/ryzom/client/windows/upgd_nl.bat
@@ -9,26 +9,27 @@ rem We used this hack because to client 2.1 to 3.0 patch,
 rem MSVC10 DLLs are using the same name and are deleted during patch
 if exist %ROOTPATH%\msvcp100_win32.dll (
 	if exist %ROOTPATH%\msvcp100.dll del %ROOTPATH%\msvcp100.dll
-	copy %ROOTPATH%\msvcp100_win32.dll %ROOTPATH%\msvcp100.dll
+	copy /Y %ROOTPATH%\msvcp100_win32.dll %ROOTPATH%\msvcp100.dll
 )
 
 if exist %ROOTPATH%\msvcr100_win32.dll (
 	if exist %ROOTPATH%\msvcr100.dll del %ROOTPATH%\msvcr100.dll
-	copy %ROOTPATH%\msvcr100_win32.dll %ROOTPATH%\msvcr100.dll
+	copy /Y %ROOTPATH%\msvcr100_win32.dll %ROOTPATH%\msvcr100.dll
 )
 
 if exist %ROOTPATH%\msvcp100_win64.dll (
 	if exist %ROOTPATH%\msvcp100.dll del %ROOTPATH%\msvcp100.dll
-	copy %ROOTPATH%\msvcp100_win64.dll %ROOTPATH%\msvcp100.dll
+	copy /Y %ROOTPATH%\msvcp100_win64.dll %ROOTPATH%\msvcp100.dll
 )
 
 if exist %ROOTPATH%\msvcr100_win64.dll (
 	if exist %ROOTPATH%\msvcr100.dll del %ROOTPATH%\msvcr100.dll
-	copy %ROOTPATH%\msvcr100_win64.dll %ROOTPATH%\msvcr100.dll
+	copy /Y %ROOTPATH%\msvcr100_win64.dll %ROOTPATH%\msvcr100.dll
 )
 
 if exist %ROOTPATH%\..\ryzom_installer_qt_r.exe (
-	copy %ROOTPATH%\ryzom_installer_qt_r.exe %ROOTPATH%\..
+	if exist %ROOTPATH%\..\ryzom_installer_qt_r.exe del %ROOTPATH%\..\ryzom_installer_qt_r.exe
+	copy /Y %ROOTPATH%\ryzom_installer_qt_r.exe %ROOTPATH%\..
 )
 
 :end
