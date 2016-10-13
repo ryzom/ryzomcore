016e075fa5f1958a59798ea21ef7a40a7262d903
kervala <none@none>
2016-10-13 19:50:11 +0200

    Changed: Only install same client arch as Installer under Linux
    --HG--
    branch : develop

--- code/ryzom/tools/client/ryzom_installer/src/migratedialog.cpp
+++ code/ryzom/tools/client/ryzom_installer/src/migratedialog.cpp
@@ -54,7 +54,9 @@ CMigrateDialog::CMigrateDialog():QDialog()
 	clientArchGroupBox->setVisible(false);
 	clientArch64RadioButton->setChecked(true);
 	clientArch32RadioButton->setChecked(false);
-#else
+#elif defined(Q_OS_WIN32)
+	// both 32 and 64 bits are working under Windows 64 bits
+
 	// check whether OS architecture is 32 or 64 bits
 	if (CConfigFile::has64bitsOS())
 	{
@@ -70,6 +72,21 @@ CMigrateDialog::CMigrateDialog():QDialog()
 		clientArch64RadioButton->setChecked(false);
 		clientArch32RadioButton->setChecked(true);
 	}
+#else
+	// only use the current architecture for Linux
+
+	clientArchGroupBox->setVisible(false);
+
+#ifdef _LP64
+	// only 64 bits is available
+	clientArch64RadioButton->setChecked(true);
+	clientArch32RadioButton->setChecked(false);
+#else
+	// only 32 bits is available
+	clientArch64RadioButton->setChecked(false);
+	clientArch32RadioButton->setChecked(true);
+#endif
+
 #endif
 
 	const CServer &server = CConfigFile::getInstance()->getServer();
