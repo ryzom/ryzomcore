084bc2bba9993dce60000a260c689dd1261fa247
kervala <none@none>
2016-10-20 13:53:23 +0200

    Changed: Allow to enable Sandbox under OS X
    --HG--
    branch : develop

--- code/CMakeModules/nel.cmake
+++ code/CMakeModules/nel.cmake
@@ -333,6 +333,7 @@ MACRO(NL_SETUP_RYZOM_DEFAULT_OPTIONS)
   OPTION(WITH_RYZOM_CLIENT_UAC    "Ask to run as Administrator"                   OFF)
   OPTION(WITH_RYZOM_PATCH         "Enable Ryzom in-game patch support"            OFF)
   OPTION(WITH_RYZOM_CUSTOM_PATCH_SERVER "Only use patch server from CFG file"     OFF)
+  OPTION(WITH_RYZOM_SANDBOX       "Enable Sandbox under OS X"                     OFF)
 ENDMACRO(NL_SETUP_RYZOM_DEFAULT_OPTIONS)
 
 MACRO(NL_SETUP_SNOWBALLS_DEFAULT_OPTIONS)
