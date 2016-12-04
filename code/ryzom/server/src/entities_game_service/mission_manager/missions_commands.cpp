bf7b51ca6cd992eac6c159d49b0c9b140bf8735f
kervala <none@none>
2016-12-04 17:11:12 +0100

    Fixed: Only 1 parameter for forceMissionProgress
    --HG--
    branch : develop

--- code/ryzom/server/src/entities_game_service/mission_manager/missions_commands.cpp
+++ code/ryzom/server/src/entities_game_service/mission_manager/missions_commands.cpp
@@ -49,7 +49,7 @@ using namespace std;
 
 NLMISC_COMMAND( forceMissionProgress,"debug command used to trigger debug commands","<user>" )
 {
-	if (args.empty() || args.size() > 3)
+	if (args.size() != 1)
 		return false;
 	CEntityId id;
 	id.fromString( args[0].c_str() );
