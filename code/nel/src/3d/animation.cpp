b6662b9e0ba677f83c601771226c01d93bbf3527
kervala <none@none>
2016-12-04 17:00:20 +0100

    Changed: Memory leaks detection
    --HG--
    branch : develop

--- code/nel/src/3d/animation.cpp
+++ code/nel/src/3d/animation.cpp
@@ -30,6 +30,9 @@
 using namespace std;
 using namespace NLMISC;
 
+#ifdef DEBUG_NEW
+#define new DEBUG_NEW
+#endif
 
 namespace NL3D
 {
--- code/ryzom/client/src/client.cpp
+++ code/ryzom/client/src/client.cpp
@@ -143,6 +143,10 @@ int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE /* hPrevInstance */, LPSTR cm
 int main(int argc, char **argv)
 #endif
 {
+#if defined(_MSC_VER) && defined(_DEBUG)
+	_CrtSetDbgFlag (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
 	// init the Nel context
 	CApplicationContext *appContext = new CApplicationContext;
 
