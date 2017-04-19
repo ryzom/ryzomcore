9586e39ca472275da0f172153007007d8e251f56
Guillaume Dupuy <glorf@glorf.fr>
2017-04-19 09:53:27 +0200

    Use stdpch.h instead for include
    --HG--
    branch : fix_compilation_windows

--- code/ryzom/server/src/entities_game_service/deposit.cpp
+++ code/ryzom/server/src/entities_game_service/deposit.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <functional>
+
 #include "stdpch.h"
 #include "deposit.h"
 #include "player_manager/character.h"
