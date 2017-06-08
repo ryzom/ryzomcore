e0ab70ca8237c738771b4f29d6fc573a6a4947d8
Nimetu <nimetu@gmail.com>
2017-06-08 22:37:19 +0300

    Fixed: Correctly expire object if file is removed from cache.
    --HG--
    branch : develop

--- code/nel/src/gui/group_html.cpp
+++ code/nel/src/gui/group_html.cpp
@@ -325,7 +325,7 @@ namespace NLGUI
 		time(&currentTime);
 
 		CHttpCacheObject cache = CHttpCache::getInstance()->lookup(download.dest);
-		if (cache.Expires > currentTime)
+		if (CFile::fileExists(download.dest) && cache.Expires > currentTime)
 		{
 	#ifdef LOG_DL
 			nlwarning("Cache for (%s) is not expired (%s, expires:%d)", download.url.c_str(), download.dest.c_str(), cache.Expires - currentTime);
