9cc97b8ebaf00867cfc620e3d0bd49e7aa470e06
ulukyn <none@none>
2018-06-06 14:15:42 +0200

    Added: getDbProp64 to get 64b values from db with lua
    --HG--
    branch : compatibility-develop

--- code/ryzom/client/src/interface_v3/lua_ihm_ryzom.h
+++ code/ryzom/client/src/interface_v3/lua_ihm_ryzom.h
@@ -120,6 +120,7 @@ private:
 	///////////////////////////// Standard Lua stuff ends here //////////////////////////////////////////////
 
 	static sint32 getDbProp(const std::string &dbProp); // return 0 if not found.
+	static sint64 getDbProp64(const std::string &dbProp); // return 0 if not found.
 	static void	setDbProp(const std::string &dbProp, sint32 value);		// Nb: the db prop is not created if not present.
 	static void	addDbProp(const std::string &dbProp, sint32 value);		// Nb: the db prop is created if not present.
 	static void	delDbProp(const std::string &dbProp);
