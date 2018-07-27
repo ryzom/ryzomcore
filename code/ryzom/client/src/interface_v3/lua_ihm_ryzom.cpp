9192b129d952b43874d73328eff7d8fcbcd335fa
ulukyn <none@none>
2018-07-27 03:12:35 +0200

    Added: getSheetFamily lua bind
    --HG--
    branch : compatibility-develop

--- code/ryzom/client/src/interface_v3/lua_ihm_ryzom.cpp
+++ code/ryzom/client/src/interface_v3/lua_ihm_ryzom.cpp
@@ -541,6 +541,7 @@ void CLuaIHMRyzom::RegisterRyzomFunctions(NLGUI::CLuaState &ls)
 		LUABIND_FUNC(isDynStringAvailable),
 		LUABIND_FUNC(isFullyPatched),
 		LUABIND_FUNC(getSheetType),
+		LUABIND_FUNC(getSheetFamily),
 		LUABIND_FUNC(getSheetName),
 		LUABIND_FUNC(getFameIndex),
 		LUABIND_FUNC(getFameName),
@@ -3314,6 +3315,19 @@ std::string CLuaIHMRyzom::getSheetType(const std::string &sheet)
 	return CEntitySheet::typeToString(sheetPtr->Type);
 }
 
+
+// ***************************************************************************
+std::string CLuaIHMRyzom::getSheetFamily(const std::string &sheet)
+{
+	CEntitySheet *pES = SheetMngr.get ( CSheetId(sheet) );
+	if ((pES != NULL) && (pES->type() == CEntitySheet::ITEM))
+	{
+		CItemSheet *pIS = (CItemSheet*)pES;
+
+		if (pIS)
+			return ITEMFAMILY::toString(pIS->Family);
+	}
+}
 
 // ***************************************************************************
 std::string CLuaIHMRyzom::getSheetName(uint32 sheetId)
