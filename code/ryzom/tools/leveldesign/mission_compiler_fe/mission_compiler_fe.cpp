14304c7f643577337e5a04eaf2210d21e3681199
kaetemi <kaetemi@gmail.com>
2019-05-01 07:05:48 +0800

    Cleaning up unicode conversion

--- code/ryzom/tools/leveldesign/mission_compiler_fe/mission_compiler_fe.cpp
+++ code/ryzom/tools/leveldesign/mission_compiler_fe/mission_compiler_fe.cpp
@@ -79,7 +79,7 @@ BOOL CMissionCompilerFeApp::InitInstance()
 #endif
 
 	// look at the command line parameters for command line mode
-	string cmdLine = m_lpCmdLine;
+	string cmdLine = NLMISC::tStrToUtf8(m_lpCmdLine);
 	if (cmdLine.find("-c") != string::npos)
 	{
 		NLMISC::createDebug();
