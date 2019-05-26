08ac7175dc9ab980c1fa1467ba7d46d9a2f62039
kaetemi <kaetemi@gmail.com>
2019-05-26 17:22:10 +0800

    This might possibly fix the XP build

--- code/nel/src/misc/stdmisc.h
+++ code/nel/src/misc/stdmisc.h
@@ -20,8 +20,11 @@
 #if defined(_MSC_VER) && defined(_DEBUG)
 	#define _CRTDBG_MAP_ALLOC
 	#include <stdlib.h>
+	#include <malloc.h>
 	#include <crtdbg.h>
 	#define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+#elif defined(_MSC_VER)
+	#include <malloc.h>
 #endif
 
 #include <algorithm>
