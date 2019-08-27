d0b3d0596cb4022e90da0fd48312474b2362d9d5
Inky <jnk@mailoo.org>
2019-08-27 21:30:07 +0300

    Fixed: undesired code introduced from merging lua branch
    --HG--
    branch : atys

--- code/ryzom/client/src/interface_v3/lua_ihm_ryzom.cpp
+++ code/ryzom/client/src/interface_v3/lua_ihm_ryzom.cpp
@@ -3293,7 +3293,6 @@ void	CLuaIHMRyzom::browseNpcWebPage(const std::string &htmlId, const std::string
 		groupHtml->setTimeout((float)std::max(0.0, timeout));
 
 		// Browse the url
-		groupHtml->clean();
 		groupHtml->browse(url.c_str());
 		// Set top of the page
 		CCtrlScroll *pScroll = groupHtml->getScrollBar();
