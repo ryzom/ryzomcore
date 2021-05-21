5614028f632d370b6f2d9d37b8651126bb54dfbc
Nimetu <nimetu@gmail.com>
2021-05-21 14:43:05 +0300

    Fix bad merge

--- ryzom/client/src/interface_v3/chat_text_manager.cpp
+++ ryzom/client/src/interface_v3/chat_text_manager.cpp
@@ -423,10 +423,6 @@ CViewBase *CChatTextManager::createMsgTextComplex(const string &msg, NLMISC::CRG
 
 	string::size_type pos = 0;
 
-	// Manage Translations
-	CCDBNodeLeaf	*node= NLGUI::CDBManager::getInstance()->getDbProp("UI:SAVE:CHAT:SHOW_TRANSLATION_ONLY_AS_TOOLTIP_CB", false);
-	bool originalFirst = node->getValueBool();
-
 	string::size_type startTr = msg.find("{:");
 	string::size_type endOfOriginal = msg.find("}@{");
 
