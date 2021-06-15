1d06892f16c2e7763ae6bf21f1f9368584508283
kaetemi <jan.boon@kaetemi.be>
2021-06-16 06:14:17 +0800

    Remove duplicate web folder

--- ryzom/tools/server/www/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$USERS_DIR 		= '/home/nevrax/live/www';				// without final /
-$TEMPLATE_DIR	= './template';		// without final /
-
-?>
--- ryzom/tools/server/www/login/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// This file contains all variables needed by other php scripts
-
-include_once('/home/nevrax/patchman/config.php');
-
-?>
