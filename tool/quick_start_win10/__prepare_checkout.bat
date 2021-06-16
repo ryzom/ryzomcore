187ef3af19b7c0e368f4c3f5814648c6917b201c
kaetemi <jan.boon@kaetemi.be>
2021-06-17 07:29:34 +0800

    Add scripts for preparing the package

---
+++ tool/quick_start_win10/__prepare_checkout.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/ryzom/ryzomcore.git code
+git clone https://github.com/ryzom/ryzomcore_leveldesign.git leveldesign
+git clone https://github.com/ryzom/ryzomcore_sound.git sound
+git clone https://github.com/ryzom/ryzomcore_patchman.git patchman
+git clone https://github.com/ryzom/ryzomcore_graphics.git graphics
+pause
