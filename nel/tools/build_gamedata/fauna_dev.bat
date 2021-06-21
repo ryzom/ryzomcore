c919b1891b0356ed6209a04a7524115ca973de37
kaetemi <jan.boon@kaetemi.be>
2021-06-22 01:47:04 +0800

    Ensure python from the path is used

--- nel/tools/build_gamedata/fauna_dev.bat
+++ nel/tools/build_gamedata/fauna_dev.bat
@@ -1,11 +1,11 @@
 title Ryzom Core: 1_export.py (FAUNA)
-1_export.py -ipj common/fauna
+python 1_export.py -ipj common/fauna
 title Ryzom Core: 2_build.py (FAUNA)
-2_build.py -ipj common/fauna
+python 2_build.py -ipj common/fauna
 title Ryzom Core: 3_install.py (FAUNA)
-3_install.py -ipj common/fauna
+python 3_install.py -ipj common/fauna
 title Ryzom Core: b1_client_dev.py (FAUNA)
-b1_client_dev.py
+python b1_client_dev.py
 title Ryzom Core: b2_shard_data.py (FAUNA)
-b2_shard_data.py
+python b2_shard_data.py
 title Ryzom Core: Ready (FAUNA)
