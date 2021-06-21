56beb7e2148305ef15be0c32500ae5035cc152e2
kaetemi <jan.boon@kaetemi.be>
2021-06-22 01:32:01 +0800

    Add some additional utility scripts for the pipeline

---
+++ nel/tools/build_gamedata/objects_dev.bat
@@ -0,0 +1,11 @@
+title Ryzom Core: 1_export.py (OBJECTS)
+1_export.py -ipj common/objects
+title Ryzom Core: 2_build.py (OBJECTS)
+2_build.py -ipj common/objects
+title Ryzom Core: 3_install.py (OBJECTS)
+3_install.py -ipj common/objects
+title Ryzom Core: b1_client_dev.py (OBJECTS)
+b1_client_dev.py
+title Ryzom Core: b2_shard_data.py (OBJECTS)
+b2_shard_data.py
+title Ryzom Core: Ready (OBJECTS)
---
+++ nel/tools/build_gamedata/sound_dev.bat
@@ -0,0 +1,9 @@
+title Ryzom Core: 1_export.py (SOUND)
+1_export.py -ipj common/sound
+title Ryzom Core: 2_build.py (SOUND)
+2_build.py -ipj common/sound
+title Ryzom Core: 3_install.py (SOUND)
+3_install.py -ipj common/sound
+title Ryzom Core: b1_client_dev.py (SOUND)
+b1_client_dev.py
+title Ryzom Core: Ready (SOUND)
