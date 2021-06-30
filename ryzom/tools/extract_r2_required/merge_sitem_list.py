03b9e1f44f5ffae029c7eed40bd306d52b7979b6
kaetemi <jan.boon@kaetemi.be>
2021-06-30 17:56:34 +0800

    Remove known missing from list

--- ryzom/tools/extract_r2_required/merge_sitem_list.py
+++ ryzom/tools/extract_r2_required/merge_sitem_list.py
@@ -16,18 +16,25 @@ def listPathExt(path, ext):
 
 listPathExt(sitemPath, ".sitem")
 
+knownMissing = {}
+with open("missing_sheets.txt", "r") as r:
+	for l in r:
+		knownMissing[l.strip()] = True
+
 with open("sitem_list.txt", "w") as w:
 	with open("sitem_missing.txt", "w") as wm:
 		with open("sitem_list_r2.txt", "r") as r:
 			for l in r:
-				w.write(l)
-				if not l.strip() in fileMap:
-					wm.write(l)
-		with open("sitem_list_wk.txt", "r") as r:
-			for l in r:
-				if len(l.strip()) > 0 and not l.startswith(";"):
+				if not l.strip() in knownMissing:
 					w.write(l)
 					if not l.strip() in fileMap:
 						wm.write(l)
+		with open("sitem_list_wk.txt", "r") as r:
+			for l in r:
+				if len(l.strip()) > 0 and not l.startswith(";"):
+					if not l.strip() in knownMissing:
+						w.write(l)
+						if not l.strip() in fileMap:
+							wm.write(l)
 		wm.flush()
 	w.flush()
