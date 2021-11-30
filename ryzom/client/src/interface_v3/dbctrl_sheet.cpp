26827d2c66d4d456474a93ba609be5abfff6504b
Nimetu <nimetu@gmail.com>
2021-11-30 14:59:46 +0200

    Fix item buff icon overlapping with quantity

--- ryzom/client/src/interface_v3/dbctrl_sheet.cpp
+++ ryzom/client/src/interface_v3/dbctrl_sheet.cpp
@@ -2534,7 +2534,8 @@ void CDBCtrlSheet::drawSheet (sint32 x, sint32 y, bool draging, bool showSelecti
 					// there is max 4 icons
 					sint32 hArea = (hSheet / 4);
 					sint32 xIcon = x;
-					sint32 yIcon = y;
+					// move buff icons up a row, quantity text is displayed on bottom-left corner
+					sint32 yIcon = y + hArea;
 					for (uint i = 0; i < _BuffIcons.size(); ++i)
 					{
 						sint32 wIcon = _BuffIcons[i].IconW;
