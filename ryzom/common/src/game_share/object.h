9fc091f99bcf9b66ddd41ba6cc40ea643c7f6cf2
Nuno <ulukyn@gmail.com>
2022-02-09 17:24:49 +0100

    Add macro checks for enable code for old gcc version (< 40800)

--- ryzom/common/src/game_share/object.h
+++ ryzom/common/src/game_share/object.h
@@ -296,13 +296,13 @@ public:
 #if defined(NL_COMP_GCC) && (GCC_VERSION < 40800)
 	virtual const char *getTypeAsString() const;
 
-	virtual bool set(const std::string& key, sint64 value) NL_OVERRIDE;
-	virtual bool set(const std::string& key, double value) NL_OVERRIDE;
-	virtual bool set(const std::string& key, const std::string&value) NL_OVERRIDE;
+	virtual bool set(const std::string& key, sint64 value);
+	virtual bool set(const std::string& key, double value);
+	virtual bool set(const std::string& key, const std::string&value);
 
-	virtual bool setObject(const std::string& key, CObject* value) NL_OVERRIDE;
+	virtual bool setObject(const std::string& key, CObject* value);
 
-	virtual CObject* clone() const NL_OVERRIDE;
+	virtual CObject* clone() const;
 
 	virtual void dump(const std::string prefix = "", uint depth = 0) const;
 
@@ -327,18 +327,18 @@ protected:
 #if defined(NL_COMP_GCC) && (GCC_VERSION < 40800)
 	virtual void doSerialize(std::string& out, CSerializeContext& context) const;
 
-	virtual bool doIsNumber() const NL_OVERRIDE;
+	virtual bool doIsNumber() const;
 
-	virtual double doToNumber() const NL_OVERRIDE;
+	virtual double doToNumber() const;
 
-	virtual bool doIsInteger() const NL_OVERRIDE;
+	virtual bool doIsInteger() const;
 
-	virtual sint64 doToInteger() const NL_OVERRIDE;
+	virtual sint64 doToInteger() const;
 
-	virtual std::string doToString() const NL_OVERRIDE;
+	virtual std::string doToString() const;
 
-	virtual void inPlaceCopyTo(CObject &dest) const NL_OVERRIDE;
-	virtual void inPlaceCopy(const CObjectNumber &src) NL_OVERRIDE;
+	virtual void inPlaceCopyTo(CObject &dest) const;
+	virtual void inPlaceCopy(const CObjectNumber &src);
 
 	virtual void visitInternal(IObjectVisitor &visitor);
 #else
