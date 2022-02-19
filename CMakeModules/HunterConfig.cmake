01881d26c3ba122b85aa7c6723bf521e73aa69dd
kaetemi <jan.boon@kaetemi.be>
2022-02-19 08:07:12 +0800

    Fix luabind under GCC 9 etc

--- CMakeModules/HunterConfig.cmake
+++ CMakeModules/HunterConfig.cmake
@@ -2,7 +2,7 @@
 
 hunter_config(luabind
   VERSION 0.9.1
-  URL  "https://github.com/ryzom/luabind/tarball/3dd7c545034219eaea322feb15903524085e94df"
-  SHA1 "27a08c61b5eab3fed78115288bcca3be9aae581d"
+  URL  "https://github.com/ryzom/luabind/tarball/0ae9bd6e40fe6c70e9d032ff096370929f58c143"
+  SHA1 "1dfabfa89ee72066118e4e28e797830e118d2a9b"
   )
 
