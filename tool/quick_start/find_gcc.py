963ff3e2f8405e062ea616f568e995659f3e4edb
kaetemi <jan.boon@kaetemi.be>
2022-04-20 17:49:54 +0800

    Speed up environment detection by running the Python script inside the Docker container

--- tool/quick_start/find_gcc.py
+++ tool/quick_start/find_gcc.py
@@ -23,8 +23,12 @@ def DockerBaseCommand(image, arch, workdir, hunter):
 		res += [ "setarch", "i686" ]
 	return res
 
+def DockerRootPath(path):
+	return "/mnt/nel/" + path
+
 def FindDockerGCC(image):
-	# FoundVisualStudio += [ { "Name": "Visual Studio " + str(VSMajor[yearVersion]) + " " + yearVersion, "DisplayName": "Visual Studio " + yearVersion + " " + edition, "Path": editionPath, "Version": VSMajor[yearVersion], "Toolset": "v140", "HasMFC": os.path.isfile("C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\atlmfc\\include\\afx.h") } ]
+	global FoundGCC
+	
 	output = None
 	try:
 		output = subprocess.check_output(['docker', 'image', 'inspect', image], stderr=subprocess.DEVNULL, text=True)
@@ -38,65 +42,36 @@ def FindDockerGCC(image):
 	inspect = json.loads(output)
 	arch = inspect[0]["Architecture"]
 	baseCmd = DockerBaseCommand(image, arch, None, None)
-	cmd = baseCmd + [ "gcc", "-dumpfullversion", "-dumpversion" ]
-	# print("TRY: " + str(cmd))
+	
+	cmd = baseCmd + [ "python3", DockerRootPath("code/tool/quick_start/dump_gcc.py") ]
 	try:
-		output = subprocess.check_output(cmd, text=True) #, stderr=subprocess.DEVNULL, text=True)
+		output = subprocess.check_output(cmd, stderr=subprocess.DEVNULL, text=True)
 	except subprocess.CalledProcessError as e:
 		# print(e.output)
-		print("FAILED: " + str(cmd))
-		return
-	except FileNotFoundError as e:
-		print("FAILED: " + str(cmd))
-		return
-	version = output.strip()
-	cmd = baseCmd + [ "cat", "/etc/os-release" ]
-	try:
-		output = subprocess.check_output(cmd, text=True) #, stderr=subprocess.DEVNULL, text=True)
-	except subprocess.CalledProcessError as e:
-		# print(e.output)
-		print("FAILED: " + str(cmd))
-		return
-	except FileNotFoundError as e:
-		print("FAILED: " + str(cmd))
-		return
-	os = dict(csv.reader(io.StringIO(output.strip()), delimiter='='))
-	cmd = baseCmd + [ "find", "/usr/lib", "-name", "libluabind.so" ]
-	try:
-		output = subprocess.check_output(cmd, text=True) #, stderr=subprocess.DEVNULL, text=True)
-	except subprocess.CalledProcessError as e:
-		output = None
-	except FileNotFoundError as e:
-		output = None
-	luaVer = None
-	if output:
-		luaSO = output.strip()
-		cmd = baseCmd + [ "ldd", luaSO ]
-		try:
-			output = subprocess.check_output(cmd, text=True) #, stderr=subprocess.DEVNULL, text=True)
-		except subprocess.CalledProcessError as e:
-			# print(e.output)
+		if "Failed" in e.output:
+			print("FAILED: PYTHON: " + json.loads(e.output)["Failed"])
+		else:
 			print("FAILED: " + str(cmd))
-			return
-		except FileNotFoundError as e:
-			print("FAILED: " + str(cmd))
-			return
-		ldd = output.strip()
-		# print(ldd)
-		# Get the Lua version that Luabind depends on
-		# If we can find that, then we assume development libraries are available
-		ldd = ldd[ldd.index("liblua"):]
-		ldd = ldd[:ldd.index(".so")]
-		if len(ldd) > 6:
-			luaVer = ldd[6:]
-	name = "GCC " + version + " (" + os["PRETTY_NAME"]
-	if luaVer:
-		name += ", Lua " + luaVer
+		return
+	except FileNotFoundError as e:
+		print("FAILED: " + str(cmd))
+		return
+	
+	res = json.loads(output.strip())
+	res["Architecture"] = arch
+	res["Image"] = image
+	
+	name = "GCC " + res["GCCVersion"] + " " + arch + " (" + res["OSRelease"]["PRETTY_NAME"]
+	if "LuaVersion" in res:
+		name += ", Lua " + res["LuaVersion"]
 	name += ")"
-	print(image + ": " + name)
+	# print(name)
+	
+	res["DisplayName"] = name
+	FoundGCC += [ res ]
 
 for image in FoundDocker:
-	print("Detect GCC in Docker image \"" + image + "\"")
+	print("Detect GCC installation in Docker image \"" + image + "\"")
 	FindDockerGCC(image)
 
 # end of file
