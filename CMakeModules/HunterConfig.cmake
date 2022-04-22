04a7f9e711272840760dbb2a8fbc31482ce30ef7
kaetemi <jan.boon@kaetemi.be>
2022-04-22 11:14:08 +0800

    Update to latest OpenAL when using Hunter, fixes link error in GCC 11, close ryzom/ryzomcore#666

--- CMakeModules/HunterConfig.cmake
+++ CMakeModules/HunterConfig.cmake
@@ -6,3 +6,7 @@ hunter_config(luabind
   SHA1 "1dfabfa89ee72066118e4e28e797830e118d2a9b"
   )
 
+hunter_config(OpenAL
+  VERSION "1.21.1"
+  URL "https://github.com/kcat/openal-soft/archive/refs/tags/1.21.1.tar.gz"
+  SHA1 "57acf4fa55180eeb16721ac5ec0df541f586df40")
