5d458af49cf6c0fd9bfb25cce6541391de9f0405
kaetemi <jan.boon@kaetemi.be>
2022-05-04 00:28:26 +0800

    Shard launch shortcuts

--- tool/quick_start/win32/install/client_dev_local_stock.bat
+++ tool/quick_start/win32/install/client_dev_local_stock.bat
@@ -1,4 +1,4 @@
 call %~dp0.nel\path_config.bat
 cd /d %RC_ROOT%\pipeline\client_dev
-set "PATH=%RC_CLIENT_DIRS_STOCK%;%RC_EXTERNAL_BIN_DIRS%;%PATH%"
+set "PATH=%RC_CLIENT_DIRS_STOCK%;%PATH%"
 start ryzom_client_dev_r.exe
---
+++ tool/quick_start/win32/install/shard_ring_stock.bat
@@ -0,0 +1,4 @@
+call %~dp0.nel\path_config.bat
+cd /d %RC_ROOT%\pipeline\shard_dev
+set "PATH=%RC_SERVER_DIRS_STOCK%;%PATH%"
+start %RC_ROOT%\external\utils\servdash start_ring.ini
