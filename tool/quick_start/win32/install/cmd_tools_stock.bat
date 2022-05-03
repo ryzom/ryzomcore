76a0f18199e6fdfe6c1c01cb61e8840ad5cf5eba
kaetemi <jan.boon@kaetemi.be>
2022-05-03 22:58:21 +0800

    Add client shortcuts, update stock directory names

--- tool/quick_start/win32/install/cmd_tools_stock.bat
+++ tool/quick_start/win32/install/cmd_tools_stock.bat
@@ -3,6 +3,6 @@ set "RC_TOOLS_DIRS=%RC_TOOLS_DIRS_STOCK%%"
 set "RC_SAMPLES_DIRS=%RC_SAMPLES_DIRS_STOCK%%"
 set "PATH=%RC_PATH%;%RC_TOOLS_DIRS%;%RC_SAMPLES_DIRS%;%PATH%"
 cd /d %RC_ROOT%\.nel\tools
-dir %RC_ROOT%\stock\nel_tools\*.exe
-dir %RC_ROOT%\stock\ryzom_tools\*.exe
+dir %RC_ROOT%\stock\nel_tools_win_x64\*.exe
+dir %RC_ROOT%\stock\ryzom_tools_win_x64\*.exe
 cmd
