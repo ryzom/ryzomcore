1239d413d4db9bfd91a2ed989fd6e02a9bfd02f6
Nuno Gonçalves (Ulukyn) <nuno@troispetits.net>
2022-07-05 17:47:36 +0100

    Set OUTPOST_NB_SQUAD_SLOTS to 6

--- ryzom/common/src/game_share/outpost.h
+++ ryzom/common/src/game_share/outpost.h
@@ -32,9 +32,9 @@ namespace OUTPOSTENUMS
 	const uint32 OUTPOST_MAX_SPAWN_ZONE = 16;			// Same as (#y) in the database SERVER:GUILD:OUTPOST:O#x:SPAWN_ZONE:#y see (database.xml)
 	const uint32 OUTPOST_MAX_SQUAD_SHOP = 16;			// Same as (#y) in the database SERVER:GUILD:OUTPOST:O#x:SQUAD_SHOP:#y see (database.xml)
 
-	const uint32 OUTPOST_NB_SQUAD_SLOTS = 12;
-	const uint32 OUTPOST_MAX_SQUAD_TRAINING = OUTPOST_NB_SQUAD_SLOTS * 2;	// Same as (#y) in the database SERVER:GUILD:OUTPOST:O#x:S:T#y see (database.xml)
-	const uint32 OUTPOST_MAX_SQUAD_SPAWNED = OUTPOST_MAX_SQUAD_TRAINING;	// Same as (#y) in the database SERVER:GUILD:OUTPOST:O#x:S:S#y see (database.xml)
+	const uint32 OUTPOST_NB_SQUAD_SLOTS = 6;
+	const uint32 OUTPOST_MAX_SQUAD_TRAINING = OUTPOST_NB_SQUAD_SLOTS * 2;	// Same as (#y) in the database SERVER:GUILD:OUTPOST:SQUADS:SP#y see (database.xml)
+	const uint32 OUTPOST_MAX_SQUAD_SPAWNED = OUTPOST_MAX_SQUAD_TRAINING;	// Same as (#y) in the database SERVER:GUILD:OUTPOST:SQUADS:T#y see (database.xml)
 
 	const uint32 OUTPOST_MAX_BUILDINGS = 4;				// Same as (#y) in the database SERVER:GUILD:OUTPOST:O#x:BUILDINGS:#y see (database.xml)
 
