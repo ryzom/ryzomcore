9e0febc00200c3e00c9c7bd5af7fb16f580949e7
bensaine <bensaine09@gmail.com>
2022-07-06 22:35:40 -0400

    Revert "fix: remove useless params in setChar3dDBfromVPX()"

--- ryzom/client/src/interface_v3/lua_ihm_ryzom.cpp
+++ ryzom/client/src/interface_v3/lua_ihm_ryzom.cpp
@@ -3933,7 +3933,7 @@ sint CLuaIHMRyzom::getCharacterSheetRegionLevel(const std::string &sheet)
 }
 
 
-float CLuaIHMRyzom::setChar3dDBfromVPX(const std::string &branch)
+float CLuaIHMRyzom::setChar3dDBfromVPX(const std::string &branch, const std::string &people, const std::string &vpa, const std::string &vpb, const std::string &vpc)
 {
 	CCharacterSummary cs;
 	cs.VisualPropA.fromString(vpa);
