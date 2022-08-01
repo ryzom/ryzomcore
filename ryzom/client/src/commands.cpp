eedb862f4544d65bf075bc63abc69bc359e27fac
Nuno Gonçalves (Ulukyn) <nuno@troispetits.net>
2022-08-01 17:06:07 +0100

    Added updatevpa, freehands and anims/skeletons for shapes in scene3d

--- ryzom/client/src/commands.cpp
+++ ryzom/client/src/commands.cpp
@@ -381,6 +381,17 @@ NLMISC_COMMAND(naked, "get naked !", "")
 	return true;
 }
 
+NLMISC_COMMAND(freeHands, "free hands !", "")
+{
+	std::string handPath = "LOCAL:INVENTORY:HAND:";
+	uint32 i;
+	for (i = 0; i < MAX_HANDINV_ENTRIES; ++i)
+	{
+		CInventoryManager::getInstance()->unequip(handPath + NLMISC::toString(i));
+	}
+	return true;
+}
+
 NLMISC_COMMAND(afk, "Set the player as 'away from keyboard'","[<custom text>]")
 {
 	string customText;
@@ -567,7 +578,7 @@ NLMISC_COMMAND(clearShape, "Remove all shapes added with the 'shape' command.", 
 		!hasPrivilegeEG())
 		return true;*/
 	#endif // FINAL_VERSION
-	
+
 	if (ShapeAddedByCommand.empty())
 	{
 		nlwarning("No shape created yet");
@@ -5392,7 +5403,7 @@ bool CUserCommand::execute(const std::string &/* rawCommandString */, const std:
 							{
 								finalArgs += "\"" + args[index++] + "\"";
 							}
-							else 
+							else
 							{
 								finalArgs += args[index++];
 							}
