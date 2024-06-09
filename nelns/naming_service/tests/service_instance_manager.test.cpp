db740b5be1ccb1e3798e099195e00f85c9f5f553
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-06-09 07:59:42 +0000

    add additional test coverage

--- nelns/naming_service/tests/service_instance_manager.test.cpp
+++ nelns/naming_service/tests/service_instance_manager.test.cpp
@@ -3,16 +3,27 @@
 
 #include <nel/misc/log.h>
 #include <nel/misc/mem_displayer.h>
+#include <nel/net/buf_sock.h>
+#include <nelns/naming_service/service_entry.h>
 #include <nelns/naming_service/service_instance_manager.h>
+#include <nelns/naming_service/variables.h>
 
+using ::std::string;
+using ::std::vector;
 using ::testing::ElementsAre;
 using ::testing::Eq;
+using ::testing::IsFalse;
+using ::testing::IsSupersetOf;
+using ::testing::IsTrue;
 using ::testing::NotNull;
 using ::testing::StartsWith;
 using ::testing::StrEq;
 
 using ::NLMISC::CLightMemDisplayer;
 using ::NLMISC::CLog;
+using ::NLNET::CInetAddress;
+using ::NLNET::InvalidSockId;
+using ::NLNET::TServiceId;
 
 TEST(CServiceInstanceManager, getInstanceShouldAssertIfNotInitialized)
 {
@@ -65,10 +76,8 @@ TEST(CServiceInstanceManager, addUniqueServiceShouldAddShardUniqueService)
 	instance.displayInfo(&log);
 	EXPECT_THAT(
 	    displayer.lockStrings(),
-	    ElementsAre(
-	        StartsWith("Restricted services:"),
-	        StartsWith("unique-shard-service-name -> only one per shard"),
-	        StartsWith("Online registered services:")));
+	    IsSupersetOf({ StartsWith("Restricted services:"),
+	        StartsWith("unique-shard-service-name -> only one per shard") }));
 }
 
 TEST(CServiceInstanceManager, addUniqueServiceShouldAddMachineUniqueService)
@@ -86,5 +95,139 @@ TEST(CServiceInstanceManager, addUniqueServiceShouldAddMachineUniqueService)
 	    ElementsAre(
 	        StartsWith("Restricted services:"),
 	        StartsWith("unique-machine-service-name -> only one per machine"),
+	        StartsWith("Online registered services:")));
+}
+
+TEST(CServiceInstanceManager, queryStartServiceShouldAddOnlineSercie)
+{
+	CServiceInstanceManager instance;
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses = { "localhost:12345" };
+	string reason;
+	CLightMemDisplayer displayer;
+	CLog log(NLMISC::CLog::LOG_ASSERT);
+	log.addDisplayer(&displayer);
+
+	EXPECT_THAT(
+	    instance.queryStartService("online-service", serviceId, addresses, reason),
+	    IsTrue());
+
+	instance.displayInfo(&log);
+	EXPECT_THAT(
+	    displayer.lockStrings(),
+	    IsSupersetOf({ StartsWith("Online registered services:"),
+	        StartsWith(serviceId.toString()) }));
+}
+
+TEST(CServiceInstanceManager, queryStartServiceShouldAddSingleShardUnqiueService)
+{
+	CServiceInstanceManager instance;
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses = { "localhost:12345" };
+	string reason;
+	CLightMemDisplayer displayer;
+	CLog log(NLMISC::CLog::LOG_ASSERT);
+	log.addDisplayer(&displayer);
+	string serviceName = "unique-shard-service-name";
+	instance.addUniqueService(serviceName, true);
+
+	EXPECT_THAT(
+	    instance.queryStartService(serviceName, serviceId, addresses, reason),
+	    IsTrue());
+
+	instance.displayInfo(&log);
+	EXPECT_THAT(
+	    displayer.lockStrings(),
+	    IsSupersetOf({ StartsWith("Online registered services:"),
+	        StartsWith(serviceId.toString()) }));
+}
+
+TEST(CServiceInstanceManager, queryStartServiceShouldNotAddAdditionalShardUnqiueService)
+{
+	RegisteredServices.clear();
+	CServiceInstanceManager instance;
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses = { "localhost:12345" };
+	string reason;
+	CLightMemDisplayer displayer;
+	CLog log(NLMISC::CLog::LOG_ASSERT);
+	log.addDisplayer(&displayer);
+	string serviceName = "unique-shard-service-name";
+	RegisteredServices.push_back(CServiceEntry(InvalidSockId, addresses, serviceName, serviceId));
+	instance.addUniqueService(serviceName, true);
+	instance.queryStartService(serviceName, serviceId, addresses, reason);
+
+	EXPECT_THAT(
+	    instance.queryStartService(serviceName, serviceId, addresses, reason),
+	    IsFalse());
+
+	EXPECT_THAT(
+	    reason,
+	    StrEq("Service unique-shard-service-name already found as 123, must be unique on shard"));
+}
+
+TEST(CServiceInstanceManager, queryStartServiceShouldAddAdditionalMachineUnqiueServiceOnDifferentMachine)
+{
+	RegisteredServices.clear();
+	CServiceInstanceManager instance;
+	TServiceId serviceId(123);
+	vector<CInetAddress> firstAddresses = { "127.0.0.1:12345" };
+	vector<CInetAddress> secondAddresses = { "127.0.0.2:12345" };
+	string reason;
+	CLightMemDisplayer displayer;
+	CLog log(NLMISC::CLog::LOG_ASSERT);
+	log.addDisplayer(&displayer);
+	string serviceName = "unique-machine-service-name";
+	RegisteredServices.push_back(CServiceEntry(InvalidSockId, firstAddresses, serviceName, serviceId));
+	instance.addUniqueService(serviceName, false);
+	instance.queryStartService(serviceName, serviceId, firstAddresses, reason);
+
+	EXPECT_THAT(
+	    instance.queryStartService(serviceName, serviceId, secondAddresses, reason),
+	    IsTrue());
+}
+
+TEST(CServiceInstanceManager, queryStartServiceShouldNotAddAdditionalMachineUnqiueService)
+{
+	RegisteredServices.clear();
+	CServiceInstanceManager instance;
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses = { "localhost:12345" };
+	string reason;
+	CLightMemDisplayer displayer;
+	CLog log(NLMISC::CLog::LOG_ASSERT);
+	log.addDisplayer(&displayer);
+	string serviceName = "unique-machine-service-name";
+	RegisteredServices.push_back(CServiceEntry(InvalidSockId, addresses, serviceName, serviceId));
+	instance.addUniqueService(serviceName, false);
+	instance.queryStartService(serviceName, serviceId, addresses, reason);
+
+	EXPECT_THAT(
+	    instance.queryStartService(serviceName, serviceId, addresses, reason),
+	    IsFalse());
+
+	EXPECT_THAT(
+	    reason,
+	    StrEq("Service unique-machine-service-name already found as 123 on same machine"));
+}
+
+TEST(CServiceInstanceManager, releaseServiceShouldRemoveOnlineSercie)
+{
+	CServiceInstanceManager instance;
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses = { "localhost:12345" };
+	string reason;
+	CLightMemDisplayer displayer;
+	CLog log(NLMISC::CLog::LOG_ASSERT);
+	log.addDisplayer(&displayer);
+	instance.queryStartService("online-service", serviceId, addresses, reason);
+
+	instance.releaseService(serviceId);
+
+	instance.displayInfo(&log);
+	EXPECT_THAT(
+	    displayer.lockStrings(),
+	    ElementsAre(
+	        StartsWith("Restricted services:"),
 	        StartsWith("Online registered services:")));
 }
