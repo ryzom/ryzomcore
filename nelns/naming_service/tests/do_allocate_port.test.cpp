0217ac0e342793b835cb74f98e6614e389f3c211
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-06-13 06:27:05 +0000

    add test coverage for doAllocatePort

--- nelns/naming_service/tests/do_allocate_port.test.cpp
+++ nelns/naming_service/tests/do_allocate_port.test.cpp
@@ -1,8 +1,18 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include <string>
+#include <vector>
+
+#include <nel/net/buf_sock.h>
+#include <nel/net/inet_address.h>
+#include <nel/net/unified_network.h>
+
 #include <nelns/naming_service/functions.h>
 #include <nelns/naming_service/variables.h>
+
+using ::std::string;
+using ::std::vector;
 
 using ::testing::Eq;
 using ::testing::IsFalse;
@@ -11,17 +21,67 @@ using ::testing::IsTrue;
 using ::testing::NotNull;
 
 using ::NLNET::CInetAddress;
+using ::NLNET::InvalidSockId;
+using ::NLNET::TServiceId;
 
-TEST(doAllocatePort, shouldStartAtMinBaseportAndWrapArroundMaxBasePort)
+class doAllocatePortTest : public testing::Test
 {
-	MinBasePort = 10;
-	MaxBasePort = 15;
-	CInetAddress addresses( "localhost:12345" );
+protected:
+	void SetUp() override
+	{
+		MinBasePort = 10;
+		MaxBasePort = 15;
+		RegisteredServices.clear();
+	}
 
-	EXPECT_THAT(doAllocatePort(addresses), Eq(10));
-	EXPECT_THAT(doAllocatePort(addresses), Eq(11));
-	EXPECT_THAT(doAllocatePort(addresses), Eq(12));
-	EXPECT_THAT(doAllocatePort(addresses), Eq(13));
-	EXPECT_THAT(doAllocatePort(addresses), Eq(14));
-	EXPECT_THAT(doAllocatePort(addresses), Eq(10));
+	void TearDown() override
+	{
+		RegisteredServices.clear();
+	}
+};
+
+TEST_F(doAllocatePortTest, shouldStartAtMinBaseport)
+{
+	CInetAddress address("localhost:12345");
+
+	EXPECT_THAT(doAllocatePort(address), Eq(10));
+}
+
+TEST_F(doAllocatePortTest, shouldIncreasePorts)
+{
+	CInetAddress address("localhost:12345");
+
+	EXPECT_THAT(doAllocatePort(address), Eq(11));
+	EXPECT_THAT(doAllocatePort(address), Eq(12));
+	EXPECT_THAT(doAllocatePort(address), Eq(13));
+}
+
+TEST_F(doAllocatePortTest, shouldWrapArroundMaxBasePort)
+{
+	CInetAddress address("localhost:12345");
+
+	EXPECT_THAT(doAllocatePort(address), Eq(14));
+	EXPECT_THAT(doAllocatePort(address), Eq(10));
+}
+
+TEST_F(doAllocatePortTest, shouldSkipPortsUsedByRegisteredServices)
+{
+	CInetAddress address("10.0.0.1:11");
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses { address };
+	string serviceName = "test-service-name";
+	RegisteredServices.emplace_back(InvalidSockId, addresses, serviceName, serviceId);
+
+	EXPECT_THAT(doAllocatePort(address), Eq(12));
+}
+
+TEST_F(doAllocatePortTest, shouldNotSkipPortIfNotUsedByRegisteredServices)
+{
+	CInetAddress address("10.0.0.1:10");
+	TServiceId serviceId(123);
+	vector<CInetAddress> addresses { address };
+	string serviceName = "test-service-name";
+	RegisteredServices.emplace_back(InvalidSockId, addresses, serviceName, serviceId);
+
+	EXPECT_THAT(doAllocatePort(address), Eq(13));
 }
