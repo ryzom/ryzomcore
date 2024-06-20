2e1994f593f21565449f3a8d4a89b6778a741202
Nuno <ulukyn@gmail.com>
2024-06-20 03:19:39 +0200

    Fix crash of IOS

--- ryzom/server/src/admin_modules/admin_log.cpp
+++ ryzom/server/src/admin_modules/admin_log.cpp
@@ -63,5 +63,5 @@ void CAdminCommandLog::display(const char *format, ...)
 	string toDisp = str;
 	toDisp = string(IDisplayer::dateToHumanString()) + string(" ") + toDisp;
 
-	Log->displayRawNL(toDisp.c_str());
+	Log->displayRawNL("%s", toDisp.c_str());
 }
