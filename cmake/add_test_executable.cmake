522bfa5e4af483ba5cae31cfc19dda96946a684a
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-07-01 13:34:07 +0000

    add target to build all test executables

--- cmake/add_test_executable.cmake
+++ cmake/add_test_executable.cmake
@@ -1,8 +1,13 @@
+add_custom_target(unit_test)
+
 function(add_test_executable name)
     add_executable(${ARGV})
     target_link_libraries("${name}"
             GTest::gtest_main
             GTest::gmock
     )
+
     add_test(NAME "${name}" COMMAND "${name}")
+
+    add_dependencies(unit_test "${name}")
 endfunction()
