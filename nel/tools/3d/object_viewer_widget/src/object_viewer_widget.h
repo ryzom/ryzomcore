27d31eee19eb7594f2d18109a20c00fc2815554d
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-08-27 17:30:37 +0000

    move object viewer widget to qt5 and remove qt4 specifics

--- nel/tools/3d/object_viewer_widget/src/object_viewer_widget.h
+++ nel/tools/3d/object_viewer_widget/src/object_viewer_widget.h
@@ -57,9 +57,7 @@ namespace NLQT
 		public IObjectViewer
 	{
 		Q_OBJECT
-#ifdef USE_QT5
 		Q_PLUGIN_METADATA(IID IObjectViewer_iid FILE "object_viewer.json")
-#endif
 		Q_INTERFACES(NLQT::IObjectViewer)
 
 	public:
@@ -169,19 +167,7 @@ namespace NLQT
 		virtual QString name() const {return ("ObjectViewerWidget");}
 
 	protected:
-#ifdef USE_QT5
 		virtual bool nativeEvent(const QByteArray &eventType, void *message, long *result);
-#else
-
-#if defined(NL_OS_WINDOWS)
-		virtual bool winEvent(MSG * message, long * result);
-#elif defined(NL_OS_MAC)
-		virtual bool macEvent(EventHandlerCallRef caller, EventRef event);
-#elif defined(NL_OS_UNIX)
-		virtual bool x11Event(XEvent *event);
-#endif
-
-#endif
 
 	private Q_SLOTS:
 		void updateRender();
