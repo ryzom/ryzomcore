ca59ef48e1f32bfadd1a31e95bf22e9ad3732046
Tobias Peters <tobias.peters@kreativeffekt.at>
2025-09-03 16:40:04 +0000

    only add compiler requirement on demand

--- cmake/add_test_executable.cmake
+++ cmake/add_test_executable.cmake
@@ -5,11 +5,13 @@ endif()
 function(add_test_executable name)
     add_executable(${ARGV})
     target_link_libraries("${name}"
+            PRIVATE
             GTest::gtest_main
             GTest::gmock
     )
 
     add_test(NAME "${name}" COMMAND "${name}")
+    target_compile_features("${name}" PRIVATE cxx_std_17)
 
     add_dependencies(unit_test "${name}")
 endfunction()
