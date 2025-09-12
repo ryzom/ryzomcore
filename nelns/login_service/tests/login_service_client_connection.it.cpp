8b43931baa0e3c32ea03dd712fe90fd37b1f54d4
Tobias Peters <tobias.peters@kreativeffekt.at>
2025-09-12 17:38:18 +0000

    move mock persistence into separate file

--- nelns/login_service/tests/login_service_client_connection.it.cpp
+++ nelns/login_service/tests/login_service_client_connection.it.cpp
@@ -12,6 +12,8 @@
 
 #include <nelns/login_service/login_service.h>
 
+#include "mock_persistence.h"
+
 using testing::AllOf;
 using testing::ElementsAre;
 using testing::Eq;
@@ -117,37 +119,6 @@ void insertConfigVariable(NLMISC::CConfigFile &configFile, const std::string &na
 	var.setAsInt(value);
 	configFile.insertVar(name, var);
 }
-
-class MockPersistence : public IPersistence
-{
-public:
-	MOCK_METHOD(void, init, (), (override));
-
-	// user
-	MOCK_METHOD((std::pair<std::optional<LoginUserProjection>, std::string>), findUserByLogin, (const std::string &login), (override));
-
-	MOCK_METHOD(std::string, authorizeUser, (sint32 uid, const NLNET::CLoginCookie &cookie), (override));
-
-	MOCK_METHOD((std::pair<std::optional<LoginUserProjection>, std::string>), createUser, (const std::string &login, const std::string &cpassword), (override));
-
-	MOCK_METHOD((std::pair<std::vector<AuthorizedUserProjection>, std::string>), findAuthorizedUsers, (), (override));
-
-	MOCK_METHOD(std::string, updateUserWaitingOnShard, (const std::string& uid, const sint32& shardid), (override));
-
-	MOCK_METHOD(std::string, logoutUserById, (const std::string& uid), (override));
-
-	MOCK_METHOD(std::string, logoutUserByCookie, (const NLNET::CLoginCookie &cookie), (override));
-
-	MOCK_METHOD((std::pair<std::optional<std::string>, std::string>), findUserLoginById, (const std::string& uid), (override));
-
-	MOCK_METHOD((std::pair<std::vector<NotOfflineUserProjection>, std::string>), findNotOfflineUsers, (), (override));
-
-
-	// shard
-	MOCK_METHOD((std::pair<std::vector<OnlineShardProjection>, std::string>), findOnlineShardsByApplication, (const std::string &application), (override));
-
-	MOCK_METHOD((std::pair<bool, std::string>), existsShardById, (const sint32& shardid), (override));
-};
 
 class CLoginService_ClientConnectionIT : public testing::Test
 {
