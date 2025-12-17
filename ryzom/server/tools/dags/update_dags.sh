0d18dbcc582a4cba80a3ff648c49da4a5fbc7c21
Nuno Gonçalves (Ulukyn) <ulukyn@ryzom.com>
2025-12-18 00:35:25 +0100

    Updates in Dags

--- ryzom/server/tools/dags/update_dags.sh
+++ ryzom/server/tools/dags/update_dags.sh
@@ -1,7 +1,20 @@
 #/bin/sh
 
 DAGS_PATH=/home/data/dagu/dags/
+
+hostname=$(hostname)
+Hostname=${hostname^}
+
+cd Ryzom
+python create_dags.py $Hostname
+cd ..
+
+cd MonitorAIS
+python create_dags.py
+cd ..
+
 cp *.yaml $DAGS_PATH
-cp Ryzom/*.yaml $DAGS_PATH
+cp $Hostname/*.yaml $DAGS_PATH
+cp Ryzom/${Hostname}_*.yaml $DAGS_PATH
 cp RTZ/*.yaml $DAGS_PATH
 cp MonitorAIS/*.yaml $DAGS_PATH
