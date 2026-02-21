2f24a0b7dd3e3834c5d657d8c96268c08e203e6f
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-02-21 19:56:18 +0100

    update hunter openal version

--- CMakeModules/HunterConfig.cmake
+++ CMakeModules/HunterConfig.cmake
@@ -4,9 +4,10 @@ hunter_config(luabind
   VERSION 0.9.1
   URL  "https://github.com/ryzom/luabind/tarball/0ae9bd6e40fe6c70e9d032ff096370929f58c143"
   SHA1 "1dfabfa89ee72066118e4e28e797830e118d2a9b"
-  )
+)
 
 hunter_config(OpenAL
-  VERSION "1.20.1"
-  URL "https://github.com/kcat/openal-soft/archive/openal-soft-1.20.1.tar.gz"
-  SHA1 "5bd59eaf9268af8c758895c4cd02de25603313e7")
+  VERSION "1.25.1"
+  URL "https://github.com/kcat/openal-soft/archive/1.25.1.tar.gz"
+  SHA1 "b4ee32a295969b0f9ce51ee71edff85a3617b993"
+)
