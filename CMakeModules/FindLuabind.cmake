e5ba86e1111c6e12dbf0f8f9990a76b3d91b2e18
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-08 22:22:00 +0100

    only create target if not exists already

--- CMakeModules/FindLuabind.cmake
+++ CMakeModules/FindLuabind.cmake
@@ -199,14 +199,16 @@ IF(LUABIND_FOUND)
 
   FIND_CORRECT_LUA_VERSION()
 
-  add_library(Luabind::Luabind UNKNOWN IMPORTED)
-  set_target_properties(
-          Luabind::Luabind
-          PROPERTIES
-          INTERFACE_INCLUDE_DIRECTORIES "${LUABIND_INCLUDE_DIR}"
-          INTERFACE_LINK_LIBRARIES
-          $<IF:$<AND:$<BOOL:${LUABIND_LIBRARY_DEBUG}>,$<CONFIG:Debug>>,${LUABIND_LIBRARY_DEBUG},${LUABIND_LIBRARY_RELEASE}>
-  )
+  if(NOT TARGET Luabind::Luabind)
+    add_library(Luabind::Luabind UNKNOWN IMPORTED)
+    set_target_properties(
+            Luabind::Luabind
+            PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${LUABIND_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES
+            $<IF:$<AND:$<BOOL:${LUABIND_LIBRARY_DEBUG}>,$<CONFIG:Debug>>,${LUABIND_LIBRARY_DEBUG},${LUABIND_LIBRARY_RELEASE}>
+    )
+  endif ()
 
   IF(NOT Luabind_FIND_QUIETLY)
     MESSAGE(STATUS "Found Luabind: ${LUABIND_LIBRARIES}")
