a0e8b89c45f9ebab05b121b0393374016b0a99fa
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-03-31 22:57:39 +0200

    enable ryzom test module interface test

--- ryzom/tools/server/test_modules/game_share_test/test_module_interface.cpp
+++ ryzom/tools/server/test_modules/game_share_test/test_module_interface.cpp
@@ -1,51 +1,47 @@
+#include <gtest/gtest.h>
 
+#include <nel/misc/types_nl.h>
+#include <nel/misc/debug.h>
+#include <nel/misc/dynloadlib.h>
+#include <nel/misc/path.h>
 
-#include "nel/misc/types_nl.h"
-#include "nel/misc/debug.h"
-#include "nel/misc/dynloadlib.h"
-#include "nel/misc/path.h"
-
-#include "nel/net/unified_network.h"
-#include "nel/net/module.h"
-#include "nel/net/module_manager.h"
-#include "nel/net/module_builder_parts.h"
-
-
-#include "src/cpptest.h"
+#include <nel/net/unified_network.h>
+#include <nel/net/module.h>
+#include <nel/net/module_manager.h>
+#include <nel/net/module_builder_parts.h>
 
 #include "test_module_itf.h"
-
 
 using namespace std;
 using namespace NLMISC;
 using namespace NLNET;
 using namespace TST_MOD_ITF;
 
-//class A
+// class A
 //{
-//public:
+// public:
 //	void getParentFromA()
 //	{
 //		getParent("A");
 //	}
 //	virtual void getParent(const char*) =0;
-//};
+// };
 //
-//class B
+// class B
 //{
-//public:
+// public:
 //	void getParentFromB()
 //	{
 //		getParent("B");
 //	}
 //	virtual void getParent(const char*) =0;
-//};
+// };
 //
 //
-//class C
+// class C
 //: public A, B
 //{
-//public:
+// public:
 //
 //	C()
 //	{
@@ -59,9 +55,9 @@ using namespace TST_MOD_ITF;
 //	}
 //};
 //
-//C	c;
+// C	c;
 //
-//class D
+// class D
 //{
 //	virtual void getParent(const char*childName)
 //	{
@@ -69,14 +65,13 @@ using namespace TST_MOD_ITF;
 //	}
 //};
 
-class CModuleServant 
-	:	public CEmptyModuleServiceBehav<CEmptyModuleCommBehav<CEmptySocketBehav<CModuleBase> > >,
-		public CTestModuleInterfaceSkel
+class CModuleServant
+    : public CEmptyModuleServiceBehav<CEmptyModuleCommBehav<CEmptySocketBehav<CModuleBase>>>,
+      public CTestModuleInterfaceSkel
 {
-	bool	_ImmediateDispatching;
+	bool _ImmediateDispatching;
 
 public:
-
 	CModuleServant()
 	{
 		CTestModuleInterfaceSkel::init(this);
@@ -88,13 +83,13 @@ public:
 		return _ImmediateDispatching;
 	}
 
-//	void onProcessModuleMessage(IModuleProxy *sender, const CMessage &message)
-//	{
-//		if (CTestModuleInterfaceSkel::onDispatchMessage(sender, message))
-//			return;
-//
-//		nlassert(false);
-//	}
+	//	void onProcessModuleMessage(IModuleProxy *sender, const CMessage &message)
+	//	{
+	//		if (CTestModuleInterfaceSkel::onDispatchMessage(sender, message))
+	//			return;
+	//
+	//		nlassert(false);
+	//	}
 
 	void noParam(NLNET::IModuleProxy *sender)
 	{
@@ -102,27 +97,25 @@ public:
 
 	uint32 twoWayInvoke(NLNET::IModuleProxy *sender, uint32 value1, uint32 value2)
 	{
-		return value1+value2;
+		return value1 + value2;
 	}
 
 	void setImmediateDispatching(bool value)
 	{
 		_ImmediateDispatching = value;
 	}
-
 };
 
 NLNET_REGISTER_MODULE_FACTORY(CModuleServant, "ModuleServant");
 
-
-class CModuleClient 
-	:	public CEmptyModuleServiceBehav<CEmptyModuleCommBehav<CEmptySocketBehav<CModuleBase> > >
+class CModuleClient
+    : public CEmptyModuleServiceBehav<CEmptyModuleCommBehav<CEmptySocketBehav<CModuleBase>>>
 {
 public:
-	TModuleProxyPtr	Servant;
-	bool			TaskRunning;
+	TModuleProxyPtr Servant;
+	bool TaskRunning;
 
-	uint32			Result;
+	uint32 Result;
 
 	void startTask()
 	{
@@ -143,7 +136,7 @@ public:
 		if (proxy == Servant)
 			Servant = NULL;
 	}
-	
+
 	void taskFunc()
 	{
 		while (Servant == NULL)
@@ -154,7 +147,7 @@ public:
 			getActiveModuleTask()->yield();
 		}
 		CTestModuleInterfaceProxy tmi(Servant);
-		
+
 		Result = tmi.twoWayInvoke(this, 32, 4);
 		TaskRunning = false;
 	}
@@ -165,82 +158,73 @@ public:
 		CTestModuleInterfaceProxy tmi(Servant);
 		Result = tmi.twoWayInvoke(this, 32, 4);
 	}
-
 };
-
 
 NLNET_REGISTER_MODULE_FACTORY(CModuleClient, "ModuleClient");
 
-
-class CModuleInterfaceTS: public Test::Suite
+class CModuleInterfaceTS : public testing::Test
 {
 
-public:
-	CModuleInterfaceTS()
+protected:
+	void SetUp() override
 	{
-		TEST_ADD(CModuleInterfaceTS::testTwoWay);
-
+		ASSERT_TRUE(NLMISC::INelContext::getInstance().isContextInitialised());
+		NLMISC::createDebug(nullptr);
 	}
 
-	void testTwoWay()
+	void TearDown() override
 	{
-		// create two modules and test the two way invocation protocol
-
-		IModuleManager::getInstance();
-		CCommandRegistry &cr = CCommandRegistry::getInstance();
-
-		cr.execute("moduleManager.createModule StandardGateway gw", InfoLog());
-		cr.execute("moduleManager.createModule ModuleServant srv", InfoLog());
-		cr.execute("moduleManager.createModule ModuleClient clt", InfoLog());
-
-		// plug the modules
-		cr.execute("srv.plug gw", InfoLog());
-		cr.execute("clt.plug gw", InfoLog());
-
-		// retreive the client module
-		IModule *mod = IModuleManager::getInstance().getLocalModule("clt");
-		TEST_ASSERT(mod != NULL);
-		CModuleClient *client = dynamic_cast<CModuleClient *>(mod);
-		TEST_ASSERT(client != NULL);
-		CModuleServant *servant =  dynamic_cast<CModuleServant *>(IModuleManager::getInstance().getLocalModule("srv"));
-		TEST_ASSERT(servant != NULL);
-
-		client->startTask();
-
-		// update the modules
-		for (uint i=0; i<10; ++i)
-		{
-			IModuleManager::getInstance().updateModules();
-			nlSleep(10);
-		}
-
-		// check that the client have received the response
-		TEST_ASSERT(client->TaskRunning == false);
-		// check that the result is ok
-		TEST_ASSERT(client->Result == 36);
-
-
-		// Retry with immediate dispatching and direct call thank to the interface gernerator
-		client->Result = 0;
-		servant->setImmediateDispatching(true);
-
-		client->callImmediat();
-		// check that the result is ok
-		TEST_ASSERT(client->Result == 36);
-
-		// cleanup
-		// detroy the modules
-		cr.execute("moduleManager.deleteModule gw", InfoLog());
-		cr.execute("moduleManager.deleteModule srv", InfoLog());
-		cr.execute("moduleManager.deleteModule clt", InfoLog());
 	}
-
 };
 
-Test::Suite *createModuleInterfaceTS()
+TEST_F(CModuleInterfaceTS, testTwoWay)
 {
-	return static_cast<Test::Suite*>(new CModuleInterfaceTS());
+	// create two modules and test the two way invocation protocol
+
+	IModuleManager::getInstance();
+	CCommandRegistry &cr = CCommandRegistry::getInstance();
+
+	cr.execute("moduleManager.createModule StandardGateway gw", InfoLog());
+	cr.execute("moduleManager.createModule ModuleServant srv", InfoLog());
+	cr.execute("moduleManager.createModule ModuleClient clt", InfoLog());
+
+	// plug the modules
+	cr.execute("srv.plug gw", InfoLog());
+	cr.execute("clt.plug gw", InfoLog());
+
+	// retreive the client module
+	IModule *mod = IModuleManager::getInstance().getLocalModule("clt");
+	ASSERT_NE(mod, nullptr);
+	CModuleClient *client = dynamic_cast<CModuleClient *>(mod);
+	ASSERT_NE(client, nullptr);
+	CModuleServant *servant = dynamic_cast<CModuleServant *>(IModuleManager::getInstance().getLocalModule("srv"));
+	ASSERT_NE(servant, nullptr);
+
+	client->startTask();
+
+	// update the modules
+	for (uint i = 0; i < 10; ++i)
+	{
+		IModuleManager::getInstance().updateModules();
+		nlSleep(10);
+	}
+
+	// check that the client have received the response
+	ASSERT_FALSE(client->TaskRunning);
+	// check that the result is ok
+	ASSERT_EQ(client->Result, 36);
+
+	// Retry with immediate dispatching and direct call thank to the interface gernerator
+	client->Result = 0;
+	servant->setImmediateDispatching(true);
+
+	client->callImmediat();
+	// check that the result is ok
+	ASSERT_EQ(client->Result, 36);
+
+	// cleanup
+	// detroy the modules
+	cr.execute("moduleManager.deleteModule gw", InfoLog());
+	cr.execute("moduleManager.deleteModule srv", InfoLog());
+	cr.execute("moduleManager.deleteModule clt", InfoLog());
 }
-
-
-
