4c16ef8207f52555c53b504e6ad9e52b88265ca3
Nuno Gonçalves (Ulukyn) <nuno@troispetits.net>
2026-07-15 14:39:36 +0100

    🏗️ main/atys-live: Build system (Hunter config)

--- CMakeModules/HunterConfig.cmake
+++ CMakeModules/HunterConfig.cmake
@@ -11,3 +11,15 @@ hunter_config(OpenAL
   URL "https://github.com/kcat/openal-soft/archive/1.24.3.tar.gz"
   SHA1 "5d311a0ed6acded10e1a5eab44de8c18ec5790c5"
 )
+
+hunter_config(Boost
+  VERSION 1.79.0
+  URL "https://archives.boost.io/release/1.79.0/source/boost_1_79_0.tar.bz2"
+  SHA1 "31209dcff292bd6a64e5e08ceb3ce44a33615dc0"
+)
+
+hunter_config(breakpad
+    VERSION 0.0.0-12ecff3-p4
+    CMAKE_ARGS
+        "DIASDK_INCLUDE_DIR=C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/DIA SDK/include"
+)
